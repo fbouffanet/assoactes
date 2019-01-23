e9e85856ff1233555a1c7da8ec4006daacf56b27
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-01-23 21:35:23 +0000

    Initial commit

---
+++ v4/InfosAGL.php
@@ -0,0 +1,26 @@
+<?php
+print("<Head>\n");
+print("<link href='Commun/Styles.css' type='text/css' rel='stylesheet'/>");
+print('<meta http-equiv="Content-Type" content="text/html; charset=cp1252" />');
+print('</Head>');
+
+print("<body>");
+//print("<div class=alignCenter><img src=\"./images/petit_bandeau_agc.jpg\"></div><br>");
+print("<div align=center><img src=\"./images/LogoAGC.jpg\"></div><br>");
+print("<div>");
+print("<div>Ce mariage filiatif a &eacute;t&eacute; depos&eacute; par une autre association :</div><br>");
+print("<div>Les Amiti&eacute;s G&eacute;n&eacute;alogiques Limousines, avec laquelle l'AGC a un accord direct de partenariat.
+</div><br>");
+print("<div>Pour obtenir cette filiation, merci d'en faire la demande &agrave; l'adresse <a href=\"mailto:agc-dir@genea16.net\">agc-dir@genea16.net</a>, en pr&eacute;cisant</div>");
+print("<div>1) les nom des &eacute;poux, date et lieux du mariage demand&eacute; (copier/coller &agrave; partir de la base)</div>");
+print("<div>2) vos NOM, pr&eacute;nom et Num&eacute;ro d'adh&eacute;rent AGC</div>");
+print("<div>Nous transmettrons cette demande et sa r&eacute;ponse d&egrave;s que possible.</div><br>");
+print("<div>G&eacute;n&eacute;@micalement</div><br>");
+print("<div>Les gestionnaires de la base AGC</div>");
+print("<form method=post action=".$_SERVER['PHP_SELF']."><br>");
+print("<div class=alignCenter><input type=button value=\"Fermer la fenetre\" onClick=\"javascript:window.close()\"></div>");
+print("<form>");
+print("</body>");
+print("</div>");
+?>
+
