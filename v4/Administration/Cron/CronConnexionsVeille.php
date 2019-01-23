9f36a879117d666d6fb61a726e6ec1d643e378e6
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-01-23 21:35:23 +0000

    Initial commit

---
+++ v4/Administration/Cron/CronConnexionsVeille.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once '/var/www/clients/client1/web12/web/v4/Commun/config.php';
+require_once '/var/www/clients/client1/web12/web/v4/Commun/constantes.php';
+require_once '/var/www/clients/client1/web12/web/v4/Commun/ConnexionBD.php';
+
+$connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
+
+$i_nb_cnx=$connexionBD->sql_select1("select count(*) from adherent where date(derniere_connexion)=date(DATE_SUB(now(), INTERVAL 1 DAY))");
+
+$connexionBD->execute_requete("insert into stats_cnx(date,nbre) values(DATE_SUB(now(),INTERVAL 1 DAY),'$i_nb_cnx')");
+
+$connexionBD->ferme();
+
+print("Script terminé<br>");
+
+?>
---
+++ v4/InfosChabatz.php
@@ -0,0 +1,24 @@
+<?php
+print("<Head>\n");
+print("<link href='Commun/Styles.css' type='text/css' rel='stylesheet'/>");
+print('<meta http-equiv="Content-Type" content="text/html; charset=cp1252" />');
+print('</Head>');
+
+print("<body>");
+//print("<div class=alignCenter><img src=\"./images/petit_bandeau_agc.jpg\"></div><br>");
+print("<div align=center><img src=\"./images/LogoAGC.jpg\"></div><br>");
+print("<div>");
+print("<div>Ce relev&eacute; de mariage a été depos&eacute; par une autre association:<br></div");
+print("<div><br>L'association Chabatz d'entrar avec laquelle l'AGC a un accord de partenariat par le biais des AGL (Amiti&eacute;s G&eacute;n&eacute;alogiques du Limousin).<br></div>");
+print("<div><br>Le relev&eacute; est un relev&eacute; de Table D&eacute;cennale, il ne comporte donc pas de filiation.<br></div>");
+print("<div><br>Pour en savoir plus, vous êtes invit&eacute;s à rentrer en contact directement avec:<br></div>");
+print("<div>Chabatz d'entrar : <a href=\"http://ray.faure.free.fr\">http://ray.faure.free.fr/</a><br></div>");
+print("<div><br>Gene@micalement</div>");
+print("<div><br>Les gestionnaires de la base AGC</div>");
+print("<form method=post action=".$_SERVER['PHP_SELF']."><br>");
+print("<div class=alignCenter><input type=button value=\"Fermer la fenetre\" onClick=\"javascript:window.close()\"></div>");
+print("<form>");
+print("</body>");
+print("</div>");
+?>
+
