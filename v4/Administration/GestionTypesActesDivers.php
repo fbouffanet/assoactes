b0bcb34129b677dcce8c695c66f5425dff073c81
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-02-28 19:30:51 +0100

    Passage en bootstrap

--- v4/Administration/GestionTypesActesDivers.php
+++ v4/Administration/GestionTypesActesDivers.php
@@ -11,18 +11,21 @@ require_once '../Commun/ConnexionBD.php';
 require_once('../Commun/PaginationTableau.php');
 require_once('../Commun/commun.php');
 
-print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"><html>');
+print('<!DOCTYPE html>');
 print("<head>");
 print("<title>Gestion des Types d'actes</title>");
 print('<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" >');
 print('<meta http-equiv="content-language" content="fr">');
-print("<link href='../Commun/Styles.css' type='text/css' rel='stylesheet'>");
+print('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+print("<link href='../css/styles.css' type='text/css' rel='stylesheet'>");
+print("<link href='../css/bootstrap.min.css' rel='stylesheet'>");
 print("<script src='../Commun/jquery-min.js' type='text/javascript'></script>");
 print("<script src='../Commun/menu.js' type='text/javascript'></script>");
 print("<link href='../Commun/jquery-ui.css' type='text/css' rel='stylesheet'>");
 print("<link href='../Commun/jquery-ui.structure.min.css' type='text/css' rel='stylesheet'>");
 print("<link href='../Commun/jquery-ui.theme.min.css' type='text/css' rel='stylesheet'> ");
 print("<script src='../Commun/jquery.validate.min.js' type='text/javascript'></script>");
+print("<script src='../js/bootstrap.min.js' type='text/javascript'></script>");
 ?>
 <script type='text/javascript'>
 $(document).ready(function() {
@@ -42,6 +45,7 @@ $(document).ready(function() {
 <?php
 print('</head>');
 print('<body>');
+print('<div class="container">');
 
 $gst_mode = empty($_POST['mode']) ? 'LISTE': $_POST['mode'] ;
 if (isset($_GET['mod']))
@@ -69,33 +73,33 @@ $gi_num_page_cour = empty($_GET['num_page']) ? 1 : $_GET['num_page'];
 function menu_liste($pconnexionBD)
 {
    global $gi_num_page_cour;
-   $NB_LIGNES_PAR_PAGE = 50;
-   print("<div class=TITRE>Gestion des types d'actes Divers</div>");
+   print('<div class="panel panel-primary">');
+   print('<div class="panel-heading">Gestion des types d\'actes Divers</div>');
+   print('<div class="panel-body">');
    print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");
 
    $st_requete = "SELECT TA.idf, CONCAT('[NULL]-',TA.idf), TA.nom, TA.sigle_nimegue, 0 FROM type_acte TA LEFT OUTER JOIN acte A ON A.idf_type_acte = TA.idf WHERE A.idf_type_acte IS NULL UNION SELECT A.idf_type_acte, CONCAT('[',A.idf_type_acte,']-',CASE WHEN TA.idf IS NULL THEN 'NULL' ELSE TA.idf END), TA.nom, TA.sigle_nimegue, count(A.idf_type_acte) FROM acte A LEFT OUTER JOIN type_acte TA ON A.idf_type_acte = TA.idf GROUP BY 3 , 1, 4 ORDER BY 3";
-   $a_liste_type_acte = $pconnexionBD->sql_select_liste1($st_requete);
+   $a_liste_type_acte = $pconnexionBD->sql_select_liste($st_requete);
    $i_nb_types_acte=count($a_liste_type_acte);
-   $i_nb_totalpages=ceil($i_nb_types_acte/$NB_LIGNES_PAR_PAGE);
    if ($i_nb_types_acte!=0)
    {        
-      $pagination = new PaginationTableau($_SERVER['PHP_SELF'],'num_page', $i_nb_types_acte, $NB_LIGNES_PAR_PAGE, $i_nb_totalpages, array('ID[A]-TA', 'Type_Acte','Sigle','Nb actes', 'Modifier','Convertir'));
+      $pagination = new PaginationTableau($_SERVER['PHP_SELF'],'num_page',$i_nb_types_acte,NB_LIGNES_PAR_PAGE,(int)($i_nb_types_acte/NB_LIGNES_PAR_PAGE),array('ID[A]-TA', 'Type_Acte','Sigle','Nb actes', 'Modifier','Convertir'));
       $pagination->init_param_bd($pconnexionBD,$st_requete);
       $pagination->init_page_cour($gi_num_page_cour);
       $pagination->affiche_entete_liens_navigation();
       $pagination->affiche_tableau_edition_remplacer();
    }
    else
-     print("<div align=center>Pas de type d'acte</div>\n");
+     print('<div class="alert alert-danger">Pas de type d\'acte</div>');
    print("</form>");
    print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");  
-   print("<div align=center><br><input type=hidden name=mode value=\"MENU_AJOUTER\">");  
-   print("<input type=submit value=\"Nouveau type\"></div>");  
+   print("<input type=hidden name=mode value=\"MENU_AJOUTER\">");  
+   print("<button type=submit class=\"btn btn-primary col-md-4 col-md-offset-4\">Nouveau type</button>");  
    print('</form>');
    print("<form id=nettoyage  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\" >");
-   print('<input type="hidden" name="mode" value="NETTOYAGE_TYPE_ACTE">');  
-   print('<div align=center><br><input type="submit" value="Supprimer les types d\'actes inutilisés"/><br></div>');   
-   print("</form>");
+   print('<input type="hidden" name="mode" value="NETTOYAGE_TYPE_ACTE">'); 
+   print("<button type=submit class=\"btn btn-primary col-md-4 col-md-offset-4\">Supprimer les types d\'actes inutilis&eacute;s</button>");      
+   print("</form></div></div>");
 }
 
 /**
@@ -106,8 +110,7 @@ function menu_liste($pconnexionBD)
  */ 
 function menu_edition($pst_type_acte,$pst_sigle,$pi_idf_type_acte)
 {
-   print("<table border=1>");
-   print("<div class=TITRE>Ajout/Modification d'un type d'acte<br></div><br><br>"); 
+   print("<table class=\"table table-bordered table-striped\">");  
    print("<tr><th>Type d'acte</th><td><input type=\"text\" maxlength=50 size=30 name=nom_type_acte value=\"$pst_type_acte\"></td></tr>");
    print("<tr><th>Sigle</th><td><input type=\"text\" maxlength=5 size=5 name=sigle_type_acte value=\"$pst_sigle\"></td></tr>");
    print("<input type=hidden name=idf_type_acte value=$pi_idf_type_acte>");
@@ -121,19 +124,20 @@ function menu_edition($pst_type_acte,$pst_sigle,$pi_idf_type_acte)
  */ 
 function menu_modifier($pconnexionBD,$pi_idf_type_acte)
 {
-	list($st_nom_type_acte)=$pconnexionBD->sql_select("select nom from type_acte where idf=".$pi_idf_type_acte);
-	list($st_sigle)=$pconnexionBD->sql_select("select sigle_nimegue from type_acte where idf=".$pi_idf_type_acte);
+	list($st_nom_type_acte,$st_sigle)=$pconnexionBD->sql_select_liste("select nom, sigle_nimegue from type_acte where idf=$pi_idf_type_acte");
+	print('<div class="panel panel-primary">');
+    print('<div class="panel-heading">Edition d\'un type d\'actes</div>');
+    print('<div class="panel-body">');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\" id=\"edition_type_acte\">");
-	print("<div align=center><input type=hidden name=mode value=MODIFIER>");
+	print("<input type=hidden name=mode value=MODIFIER>");
 	print("<input type=hidden name=idf_type_acte value=$pi_idf_type_acte>");
 	menu_edition($st_nom_type_acte,$st_sigle,$pi_idf_type_acte);
-	print("</div>");
-	print("<div align=center><br><input type=submit value=\"Modifier\"></div>");
+	print("<button type=submit class=\"btn btn-primary col-md-4 col-md-offset-4\">Modifier</button>");  
 	print('</form>');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");
-	print("<div align=center><input type=hidden name=mode value=LISTE>");
-	print("<br><input type=submit value=\"Annuler\"></div>");
-	print('</form>');
+	print("<input type=hidden name=mode value=LISTE>");
+	print("<button type=submit class=\"btn btn-primary col-md-4 col-md-offset-4\">Annuler</button>");
+	print('</form></div></div>');
 }
 
 /** Affiche liste des types d'actes pour sélectionner celui qui remplacera le type sélectionné
@@ -142,15 +146,18 @@ function menu_remplacer($pconnexionBD,$pi_idf_type_acte)
 {
 	print("\r\n<form name='remplacertype' action=\"".$_SERVER['PHP_SELF']."\" method=\"post\" onSubmit=\"return 1\">");
 	print("<input type=hidden name=idf_type_acte value=$pi_idf_type_acte>");
-	print("\r\n<div align=center><input type=hidden name=mode value=\"REMPLACER\">");
+	print("\r\n<input type=hidden name=mode value=\"REMPLACER\">");
 
 	list($st_nom_type_acte,$st_sigle)=$pconnexionBD->sql_select_liste("select nom, sigle_nimegue from type_acte where idf=$pi_idf_type_acte");
-	print("\r\n<table border=1>\r\n");
-	print("<div class=TITRE>Remplacement d'un type d'acte<br></div><br><br>"); 
+	print('<div class=panel">');
+    print('<div class="panel-heading">Remplacement d\'un type d\'acte</div>');
+    print('<div class="panel-body">');
+	print("\r\n<table class=\"table table-bordered table-striped\">\r\n");
+	 
 	print("<tr><th>IDF</th><td>$pi_idf_type_acte</td></tr>");
 	print("<tr><th>Sigle</th><td>$st_sigle</td></tr>");
 	print("<tr><th>Type d'acte</th><td>$st_nom_type_acte</td></tr>");
-	print("<tr><th>Remplacer par</th><td>\r\n<select name=\"NouveauTypeActe\">\r\n");
+	print("<tr><th>Remplacer par</th><td>\r\n<select name=\"NouveauTypeActe\" class=\"form-control\">\r\n");
 	$listetypes=$pconnexionBD->liste_valeur_par_clef("select idf, nom from type_acte order by nom");
 	foreach ($listetypes as $idf=>$nom)
 	{
@@ -164,26 +171,26 @@ function menu_remplacer($pconnexionBD,$pi_idf_type_acte)
 	print("</select></td></tr>\r\n");
 	print("</table>\r\n");
 	print("</div>\r\n");
-	print("<div align=center><br><input type=submit value=\"Remplacer\" ></div>");
+	print('<button type=submit class="btn btn-primary col-md-4 col-md-offset-4">Remplacer</button>');
+
 	print('</form>');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");
 	print("<div align=center><br><input type=hidden name=mode value=LISTE>");
-	print("<input type=submit value=\"Annuler\"></div>");
-	print('</form>');
+	print('<button type=submit class="btn btn-primary col-md-4 col-md-offset-4">Annuler</button>');
+	print('</form></div></div>');
 }
 /** Affiche le formulaire d'ajout d'un type d'acte
  */ 
 function menu_ajouter($pi_idf_type_acte)
 {
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\" id=\"edition_type_acte\" >");
-	print("<div align=center><input type=hidden name=mode value=\"AJOUTER\">");
+	print("<input type=hidden name=mode value=\"AJOUTER\">");
 	menu_edition('','',$pi_idf_type_acte);
-	print("</div>");
-	print("<div align=center><br><input type=submit value=\"Ajouter\" ></div>");
+	print('<button type=submit class="btn btn-primary col-md-4 col-md-offset-4">Ajouter</button>');
 	print('</form>');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");
-	print("<div align=center><br><input type=hidden name=mode value=LISTE>");
-	print("<input type=submit value=\"Annuler\"></div>");
+	print("<input type=hidden name=mode value=LISTE>");
+	print('<button type=submit class="btn btn-primary col-md-4 col-md-offset-4">Annuler</button>');
 	print('</form>');
 }
 
@@ -239,15 +246,15 @@ switch ($gst_mode)
   case 'NETTOYAGE_TYPE_ACTE':
       $st_requete = "delete from type_acte where idf not in (select distinct idf_type_acte from acte) and idf!=".IDF_UNION;
       //print("Req=$st_requete");
-      print("<div align=center> Nettoyage effectu&eacute;</div>");
+      print("<div class=\"alert alert-successfull\"> Nettoyage effectu&eacute;</div>");
       $connexionBD->execute_requete($st_requete);
       print("<form action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">"); 
-      print('<input type="hidden" name="mode" value="LISTE"/><br>');   
-      print("<div align=center><input type=submit value=\"Retour vers la gestion des types d'acte\"></div>");
+      print('<input type="hidden" name="mode" value="LISTE"/><br>'); 
+      print("<button type=submit class=\"btn btn-primary col-md-4 col-md-offset-4\">Retour vers la gestion des types d'acte</button>");	  
       print("</form>"); 
    break;  
 }  
 
-print('</body></html>');
+print('</div></body></html>');
 
 ?>
