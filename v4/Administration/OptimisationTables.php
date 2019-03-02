8adf290d4dfd6cc999d39980a58882df55761cc8
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-03-02 13:54:18 +0100

    Passage en bootstrap

--- v4/Administration/OptimisationTables.php
+++ v4/Administration/OptimisationTables.php
@@ -15,12 +15,15 @@ require_once '../Commun/ConnexionBD.php';
  * @param array $pa_tables Liste des tables à optimiser
  */
 function affiche_menu($pa_tables) {
-   print('<div CLASS=TITRE>Optimisation des tables MySql</div>');
-   print('<div class=alignCenter><br>');
+   print('<div class="panel panel-primary">');
+   print('<div class="panel-heading">Optimisation des tables MySql</div>');
+   print('<div class="panel-body">');
+   print('<div class="align-center">');
    print(implode('<br>',$pa_tables));
-   print('<br>');
+   print('</div>');
    print('<input type=hidden name=mode value="OPTIMISATION"></div>');
-   print('<div class=alignCenter><br><input type="submit" value="Lancer l\'optimisation"/></div>');
+   print('<button type="submit" class="btn btn-primary col-md-4 col-md-offset-4">Lancer l\'optimisation</button>');
+   print('</div></div>');
 }
 
 /**
@@ -29,27 +32,31 @@ function affiche_menu($pa_tables) {
  * @param array $pa_tables Liste des tables à optimiser  
  */
 function optimise_tables($pconnexionBD,$pa_tables) {
-   print('<div class=alignCenter>');
+   print('<div class="align-center">');
    foreach ($pa_tables as $st_table) {
       $etape_prec = getmicrotime();
       $pconnexionBD->execute_requete("optimize table `$st_table`");
       print benchmark("Optimisation de la table $st_table");
    }
+   print('</div>');
    print('<input type=hidden name=mode value="FORMULAIRE">');
-   print("<br><input type=submit value=\"Menu Optimisation\"></div>");
-   print('</div>');  
+   print('<button type="submit" class="btn btn-primary col-md-4 col-md-offset-4">Menu Optimisation</button>');
+     
 } 
 
-print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"><html>');
+print('<!DOCTYPE html>');
 print("<head>");
 print('<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" >');
 print('<meta http-equiv="content-language" content="fr">');
 print("<title>Optimisation des tables</title>");
-print("<link href='../Commun/Styles.css' type='text/css' rel='stylesheet'>");
+print('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+print("<link href='../css/styles.css' type='text/css' rel='stylesheet'>");
+print("<link href='../css/bootstrap.min.css' rel='stylesheet'>");
 print("<script src='../Commun/jquery-min.js' type='text/javascript'></script>");
-print("<script src='../Commun/menu.js' type='text/javascript'></script>");
+print("<script src='../js/bootstrap.min.js' type='text/javascript'></script>");
 print('</head>');
 print('<body>');
+print('<div class="container">');
 
 $connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
 require_once("../Commun/menu.php");
@@ -73,7 +80,7 @@ switch($gst_mode)
 }
 
 print('</form>');
-print('</body></html>');
+print('</div></body></html>');
 
 
 ?>
