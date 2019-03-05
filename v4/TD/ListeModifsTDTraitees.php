0c42af703f32ce5fdac85a1b6b568fde18b3e629
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-03-05 19:02:27 +0100

    Passage en bootstrap

--- v4/TD/ListeModifsTDTraitees.php
+++ v4/TD/ListeModifsTDTraitees.php
@@ -80,7 +80,7 @@ if (isset($_SESSION['ident']))
         
       }
       print("</select></div>");
-      print ("<div class=\"input-group\"><label for=\"type_acte\">Type:</label><select name=type_acte id=type_acte> class=\"form-control\"");      
+      print ("<div class=\"input-group\"><label for=\"type_acte\">Type:</label><select name=type_acte id=type_acte class=\"form-control\">");      
       foreach ($ga_types as $i_type => $st_lib_type)
       {
          if ($i_type==$gi_type_acte)
@@ -120,7 +120,7 @@ if (isset($_SESSION['ident']))
           $st_action = "<form action=\"".$_SERVER['PHP_SELF']."\" method=post>";
           $st_action .="<input type=\"hidden\" name=\"idf_modif\" value=\"$i_idf_modif\">";
           $st_action .="<input type=\"hidden\" name=\"mode\" value=\"VISU_MODIF\">";
-		 $st_action .= '<button type="submit" class="btn btn-primary">Voir la demande</button>';
+		    $st_action .='<button type="submit" class="btn btn-primary">Voir la demande</button>';
           $st_action .= "</form>";
           if ($gst_statut == 'A')
             $a_tableau_a_afficher[] = array($st_date,$st_type,$st_commune,$st_parties,$st_date_demande,$st_demandeur,$st_date_validation,$st_valideur,$st_action,"<a href=\"../InfosTD.php?idf_acte=$i_idf_acte\" target=\"_blank\" class=\"btn btn-primary\" role=\"button\">Voir la modification</a>");
