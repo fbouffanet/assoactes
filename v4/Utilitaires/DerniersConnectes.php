61f62b8e859c801bc02b3bdcd537d442a8377d7f
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-03-14 08:06:45 +0100

    Passage en bootstrap

--- v4/Utilitaires/DerniersConnectes.php
+++ v4/Utilitaires/DerniersConnectes.php
@@ -9,14 +9,16 @@ require_once '../Commun/commun.php';
 require_once('../Commun/ConnexionBD.php');
 require_once('../Commun/PaginationTableau.php');
 
-print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"><html>');
+print('<!DOCTYPE html>');
 print("<head>");
 print('<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" >');
 print('<meta http-equiv="content-language" content="fr">');
-print("<link href='../Commun/Styles.css' type='text/css' rel='stylesheet'>");
-print("<script src='../Commun/jquery-min.js' type='text/javascript'></script>");
-print("<script src='../Commun/menu.js' type='text/javascript'></script>");
+print('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+print("<link href='../css/styles.css' type='text/css' rel='stylesheet'>");
+print("<link href='../css/bootstrap.min.css' rel='stylesheet'>");
+print("<script src='../js/jquery-min.js' type='text/javascript'></script>");
 print('<title>Base AGC: Dernières connexions</title>');
+print("<script src='../js/bootstrap.min.js' type='text/javascript'></script>");
 print('</head>');
 
 /******************************************************************************/
@@ -25,6 +27,7 @@ print('</head>');
 
 
 print("<body>");
+print('<div class="container">');
 $connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
 require_once("../Commun/menu.php");
 
@@ -52,20 +55,22 @@ $st_requete = "SELECT count(*)
 FROM `adherent`
 WHERE `derniere_connexion` IS NOT NULL";
 $i_nbadh_connectes=$connexionBD->sql_select1($st_requete);
-print("<div class=TITRE>$i_nbadh_connectes adh&eacute;rents se sont connect&eacute;s depuis le d&eacute;but de V4<br></div>");
 
 $st_requete = "SELECT count(*) 
 FROM `adherent`
 WHERE date(`derniere_connexion`) = date(now())";
 $i_nbadh_connectes_aujourdhui=$connexionBD->sql_select1($st_requete);
-print("<div align=center><br>$i_nbadh_connectes_aujourdhui adh&eacute;rents se sont connect&eacute;s aujourd'hui <br></div>");
+print('<div class="panel panel-primary">');
+print("<div class=\"panel-heading\">$i_nbadh_connectes adh&eacute;rents se sont connect&eacute;s depuis le d&eacute;but de V4</div>");
+print('<div class="panel-body">');
+print("<div class=\"alert alert-info\">$i_nbadh_connectes_aujourdhui adh&eacute;rents se sont connect&eacute;s aujourd'hui</div>");
 
-//$st_requete = "SELECT date_format(date,'%d/%m/%Y'),nbre from `stats_cnx` order by date desc limit 7";
+
 $st_requete = "SELECT date_format(date,'%d/%m'),nbre from `stats_cnx` order by date desc limit 7";
 $a_nb_dernieres_cnx = $connexionBD->liste_valeur_par_clef($st_requete);
 if (count($a_nb_dernieres_cnx>0))
 {
-   print("<div align=center><br><table border=1>");
+   print("<table class=\"table table-bordered table-striped\">");
    print("<tr>");
    print("<th>Date</th>");
    foreach (array_keys($a_nb_dernieres_cnx) as $st_date)
@@ -80,7 +85,7 @@ if (count($a_nb_dernieres_cnx>0))
       print("<td>$i_nbre</td>");
    }
    print("</tr>");
-   print("</table><br></div>");
+   print("</table>");
 }
 
 switch($gst_mode_tri)
@@ -112,5 +117,6 @@ foreach ($ga_cnx as $a_ligne)
 $pagination->init_page_cour($gi_num_page_cour);
 $pagination->affiche_entete_liens_navigation();
 $pagination->affiche_tableau_simple($ga_tableau);
-print("</body></html>");
+print("</div></div>");
+print("</div></body></html>");
 ?>
