75539d6a3f178c1fc0017aea9b2e53ec4a7842d8
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-03-19 23:39:24 +0100

    Correction syntaxe & requête

--- v4/ajax/variantes_patro.php
+++ v4/ajax/variantes_patro.php
@@ -17,39 +17,33 @@ if (isset ($_GET['term']))
     $st_rech = substr(trim($_GET['term']),0,35);  
   $connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
   $st_rech=utf8_decode($st_rech); 
-  //$st_rech="$st_rech%";
+  $st_rech="$st_rech%";
    
   $connexionBD->initialise_params(array(':recherche'=>$st_rech));
-  $st_requete = "select idf_groupe,patronyme,majeure from variantes_patro like :recherche  order by majeure desc";
+  $st_requete = "select vp1.idf_groupe,vp1.patronyme,vp1.majeure from variantes_patro vp1 join variantes_patro vp2 on (vp1.idf_groupe=vp2.idf_groupe) where vp2. patronyme like :recherche  order by majeure desc";
   $a_patros = $connexionBD->sql_select_multiple($st_requete);
   $a_variantes_patros = array();
   $st_majeure= '';
   if (count($a_patros)>0)
   {	
-    list($i_idf_groupe,$st_majeure,$st_majeure) = array_shift(($a_patros);  
+    list($i_idf_groupe,$st_majeure,$st_majeure) = array_shift($a_patros);  
 	foreach ($a_patros as  $a_ligne)
 	{
      list($i_idf_groupe,$st_patronyme,$st_majeure)  =  $a_ligne;
-	   $a_variantes_patros[]=utf8_encode($st_patronyme);
-     if (array_key_exists($i_idf_groupe,$a_resultats))
-         $a_resultats[$i_idf_groupe][]=utf8_encode($st_patronyme);         
-     else
-         $a_resultats[$i_idf_groupe]=array(utf8_encode($st_patronyme));       
+	   $a_variantes_patros[]=utf8_encode($st_patronyme);       
 	}
   }  
 }
 $a_retour = array();
-$i_nb_resultats = count($a_resultats);
+$i_nb_resultats = count($a_variantes_patros);
 $a_retour['nb_reponses']= $i_nb_resultats;
 if ($i_nb_resultats==1)
 {
-  $a_clefs = array_keys($a_resultats);
-  $a_valeurs = array_values($a_resultats);
-  $a_retour['idf_groupe']=$a_clefs[0];
+  $a_retour['idf_groupe']=$i_idf_groupe;
   $a_retour['majeure']=$st_majeure;
-  $a_retour['variantes']=array_unique($a_valeurs[0]);
+  $a_retour['variantes']= $a_variantes_patros;
 }
 else
-  $a_retour['variantes']=$a_variantes_prenom;	
+  $a_retour['variantes']=$a_variantes_patros;	
 echo json_encode($a_retour); 
 ?>
