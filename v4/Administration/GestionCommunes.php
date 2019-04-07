31d77c3ec819d6ef50763dcd67311268b2026b41
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-08 00:43:31 +0200

    Validation suppression communes en couleur

--- v4/Administration/GestionCommunes.php
+++ v4/Administration/GestionCommunes.php
@@ -125,6 +125,23 @@ $("#suppression_communes").validate({
   messages: {
      "supp[]": "Merci de choisir au moins une commune à supprimer"
   },
+  errorElement: "em",
+  errorPlacement: function ( error, element ) {
+	// Add the `help-block` class to the error element
+	error.addClass( "help-block" );
+
+	if ( element.prop( "type" ) === "checkbox" ) {
+		error.insertAfter( element.parent( "label" ) );
+	} else {
+		error.insertAfter( element );
+	}
+	},
+	highlight: function ( element, errorClass, validClass ) {
+		$( element ).parents( ".lib_erreur" ).addClass( "has-error" ).removeClass( "has-success" );
+	},
+	unhighlight: function (element, errorClass, validClass) {
+		$( element ).parents( ".lib_erreur" ).addClass( "has-success" ).removeClass( "has-error" );
+	},
   submitHandler: function(form) {
     var communes='';
     $("input:checkbox").each(function(){
