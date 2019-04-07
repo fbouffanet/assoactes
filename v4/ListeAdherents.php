705878c6b349c4ff4d18e6669ef3e9ff696dbef1
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-04-07 09:17:09 +0200

    Correction suppression

--- v4/ListeAdherents.php
+++ v4/ListeAdherents.php
@@ -264,39 +264,25 @@ $(document).ready(function() {
       "supp[]": "Merci de choisir au moins un adhérent à supprimer"
     },
     /*errorElement: "em",
-		errorPlacement: function ( error, element ) {
-			// Add the `help-block` class to the error element
-			error.addClass( "help-block" );
+				errorPlacement: function ( error, element ) {
+					// Add the `help-block` class to the error element
+					error.addClass( "help-block" );
 
-			// Add `has-feedback` class to the parent div.form-group
-			// in order to add icons to inputs
-			element.parents( ".lib_erreur" ).addClass( "has-feedback" );
-
-			if ( element.prop( "type" ) === "checkbox" ) {
-				error.insertAfter( element.parent( "label" ) );
-			} else {
-				error.insertAfter( element );
-			}
-
-			// Add the span element, if doesn't exists, and apply the icon classes to it.
-			if ( !element.next( "span" )[ 0 ] ) {
-				$( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
-			}
-		},
-		success: function ( label, element ) {
-			// Add the span element, if doesn't exists, and apply the icon classes to it.
-			if ( !$( element ).next( "span" )[ 0 ] ) {
-				$( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
-			}
-		},
-		highlight: function ( element, errorClass, validClass ) {
-			$( element ).parents( ".lib_erreur" ).addClass( "has-error" ).removeClass( "has-success" );
-			$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
-		},
-		unhighlight: function ( element, errorClass, validClass ) {
-			$( element ).parents( ".lib_erreur" ).addClass( "has-success" ).removeClass( "has-error" );
-			$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
-		},*/
+					if ( element.prop( "type" ) === "checkbox" ) {
+						error.insertAfter( element.parent( "label" ) );
+					} else {
+						error.insertAfter( element );
+					}
+				},
+				highlight: function ( element, errorClass, validClass ) {
+					$( element ).parents( ".lib_erreur" ).addClass( "has-error" ).removeClass( "has-success" );
+				},
+				unhighlight: function (element, errorClass, validClass) {
+					$( element ).parents( ".lib_erreur
+          
+          " ).addClass( "has-success" ).removeClass( "has-error" );
+				},
+        */
   submitHandler: function(form) {
     var adherents='';
     var nb_adherents=0;
