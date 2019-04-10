0db51a546a6ce8cb4d547a04b7846482d83ae969
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-10 23:37:42 +0200

    Ajout d'un badge sur le nombre d'occurrences

--- v4/ReponsesLiasseSimple.php
+++ v4/ReponsesLiasseSimple.php
@@ -318,11 +318,11 @@ $a_liasses=$connexionBD->sql_select_multiple($gst_requete_liasses);
 print benchmark("Recherche ");
 
 $i_nb_liasses = count($a_liasses);
-print("<div class=\"text-center\">$i_nb_liasses occurrences trouv&eacute;es.</div>");
+print("<div class=\"text-center\"><span class=\"badge\">$i_nb_liasses</span> occurrences trouv&eacute;es.</div>");
 print('<div id="curseur" class="infobulle"></div>');
 if ($i_nb_liasses>$gi_nb_max_reponses)
 {
-	print("Seules les $gi_nb_max_reponses premi&egrave;res sont affich&eacute;es");
+	print("<div class=\"text-center\">Seules les $gi_nb_max_reponses premi&egrave;res sont affich&eacute;es</div>");
 	$a_liasses = array_slice($a_liasses,0,$gi_nb_max_reponses);
 }
 print("</div>");
