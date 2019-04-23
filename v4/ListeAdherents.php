03a56d24e471f4d4b92fd2c89cce753f3c914628
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-24 00:05:17 +0200

    Ajout du style bootstrap pour select2

--- v4/ListeAdherents.php
+++ v4/ListeAdherents.php
@@ -50,11 +50,12 @@ print("<link href='css/bootstrap.min.css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.structure.min.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.theme.min.css' type='text/css' rel='stylesheet'> ");
+print("<link href='css/select2.min.css' type='text/css' rel='stylesheet'>");
+print("<link href='css/select2-bootstrap.min.css' type='text/css' rel='stylesheet'>");
 print("<script src='js/jquery-min.js' type='text/javascript'></script>");
 print("<script src='js/jquery-ui.min.js' type='text/javascript'></script>");
 print("<script src='js/jquery.validate.min.js' type='text/javascript'></script>\n");
 print("<script src='js/additional-methods.min.js' type='text/javascript'></script>\n");
-print("<link href='css/select2.min.css' type='text/css' rel='stylesheet'>");
 print("<script src='js/select2.min.js' type='text/javascript'></script>");
 print("<script src='js/bootstrap.min.js' type='text/javascript'></script>");
 print("<script type='text/javascript'>");
@@ -67,6 +68,8 @@ $(document).ready(function() {
     minLength: 3
   });
 
+  $.fn.select2.defaults.set( "theme", "bootstrap" );
+  
   $(".js-select-avec-recherche").select2();
   
   $.validator.addMethod('cotisation_statut', function(value, element) {
