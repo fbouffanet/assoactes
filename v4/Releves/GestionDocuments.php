7575e30620af94ead74b0e0c7aee36f324357bba
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-24 00:12:16 +0200

    Ajout du style bootstrap pour select2

--- v4/Releves/GestionDocuments.php
+++ v4/Releves/GestionDocuments.php
@@ -25,6 +25,7 @@ print("<link href='../css/jquery-ui.css' type='text/css' rel='stylesheet'>");
 print("<link href='../css/jquery-ui.structure.min.css' type='text/css' rel='stylesheet'>");
 print("<link href='../css/jquery-ui.theme.min.css' type='text/css' rel='stylesheet'> ");
 print("<link href='../css/select2.min.css' type='text/css' rel='stylesheet'>");
+print("<link href='../css/select2-bootstrap.min.css' type='text/css' rel='stylesheet'>");
 print("<script src='../js/jquery-min.js' type='text/javascript'></script>");
 print("<script src='../js/jquery.validate.min.js' type='text/javascript'></script>");
 print("<script src='../js/jquery-ui.min.js' type='text/javascript'></script>");
@@ -35,6 +36,8 @@ print("<script src='../js/select2.min.js' type='text/javascript'></script>");
 ?>
 <script type='text/javascript'>
 $(document).ready(function() {
+$.fn.select2.defaults.set( "theme", "bootstrap" );
+	
 $(".js-select-avec-recherche").select2();
 
 $('#commune_a_chercher').autocomplete({
