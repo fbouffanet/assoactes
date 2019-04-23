66972a2943b8071c251d11d94aaac55d2b952e0b
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-23 13:43:07 +0200

    Style bootstrap pour select2

--- v4/Recherches.php
+++ v4/Recherches.php
@@ -16,7 +16,8 @@ print("<link href='css/bootstrap.min.css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.structure.min.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.theme.min.css' type='text/css' rel='stylesheet'> ");
-print("<link href='css/select2.min.css' type='text/css' rel='stylesheet'> ");
+print("<link href='css/select2.min.css' type='text/css' rel='stylesheet'>");
+print("<link href='css/select2-bootstrap.min.css' type='text/css' rel='stylesheet'>");
 print("<script src='js/jquery-min.js' type='text/javascript'></script>");
 print("<script src='js/jquery.validate.min.js' type='text/javascript'></script>");
 print("<script src='js/additional-methods.min.js' type='text/javascript'></script>");
@@ -70,6 +71,8 @@ $(document).ready(function() {
         minLength: 3
     });
 
+	$.fn.select2.defaults.set( "theme", "bootstrap" );
+	
     $(".js-select-avec-recherche").select2();
 
     $.validator.addMethod('plusGrand', function(value, element, param) {
