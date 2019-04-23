9b5fab7a07784c49f33b803cd01855383b79e9cc
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-23 23:57:48 +0200

    Ajout du style bootstrap pour select2

--- v4/Administration/SuppressionDonnees.php
+++ v4/Administration/SuppressionDonnees.php
@@ -38,6 +38,7 @@ print("<link href='../css/jquery-ui.css' type='text/css' rel='stylesheet'>");
 print("<link href='../css/jquery-ui.structure.min.css' type='text/css' rel='stylesheet'>");
 print("<link href='../css/jquery-ui.theme.min.css' type='text/css' rel='stylesheet'> ");
 print("<link href='../css/select2.min.css' type='text/css' rel='stylesheet'> ");
+print("<link href='../css/select2-bootstrap.min.css' type='text/css' rel='stylesheet'> ");
 print("<script src='../js/jquery-min.js' type='text/javascript'></script>");
 print("<script src='../js/jquery.validate.min.js' type='text/javascript'></script>");
 print("<script src='../js/additional-methods.min.js' type='text/javascript'></script>");
@@ -47,6 +48,9 @@ print("<script src='../js/bootstrap.min.js' type='text/javascript'></script>");
 print("<script type='text/javascript'>");
 ?>
 $(document).ready(function() {
+
+  $.fn.select2.defaults.set( "theme", "bootstrap" );
+  
   $(".js-select-avec-recherche").select2();
   
   $.validator.addMethod('plusGrand', function(value, element, param) {
