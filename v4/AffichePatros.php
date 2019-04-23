8eb048d643f33a8d56fd3e118c34f8130b08db43
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-04-23 23:59:20 +0200

    Ajout du style bootstrap pour select2

--- v4/AffichePatros.php
+++ v4/AffichePatros.php
@@ -20,6 +20,7 @@ print("<link href='css/jquery-ui.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.structure.min.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/jquery-ui.theme.min.css' type='text/css' rel='stylesheet'> ");
 print("<link href='css/select2.min.css' type='text/css' rel='stylesheet'> ");
+print("<link href='css/select2-bootstrap.min.css' type='text/css' rel='stylesheet'> ");
 print("<script src='js/jquery-min.js' type='text/javascript'></script>");
 print("<script src='js/jquery-ui.min.js' type='text/javascript'></script>");
 print("<script src='js/select2.min.js' type='text/javascript'></script>");
@@ -29,8 +30,10 @@ print("<script src='js/bootstrap.min.js' type='text/javascript'></script>");
 print("<script type='text/javascript'>");
 ?>
 $(document).ready(function() {
-  $(".js-select-avec-recherche").select2();
 
+  $.fn.select2.defaults.set( "theme", "bootstrap" );
+  
+  $(".js-select-avec-recherche").select2();
 
 $("#patros").validate({
   rules: {
