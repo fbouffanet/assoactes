a13fa1ea8ae62c1ab6b2588d1ab352ca54397b36
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-05-04 17:39:27 +0200

    Suppression du load data

--- v4/Administration/chargement/TypeActe.php
+++ v4/Administration/chargement/TypeActe.php
@@ -25,31 +25,33 @@ class TypeActe {
          $this->a_type_acte[strval($pst_type_acte)]= $pst_sigle_acte;
   }
   
-  public function sauve($pst_rep_tmp,$pst_parametres_load_data) {
-     global $gst_jeu_de_caracteres_par_defaut;
-     $st_fich_temp = tempnam ($pst_rep_tmp, "type_acte.csv");
-     $pf=@fopen($st_fich_temp,"w");
-     if ($pf===FALSE)
-        throw new Exception("Ecriture fichier type_acte.csv impossible");
-     foreach ($this->a_type_acte as $st_type_acte => $st_sigle )
+  public function sauve() {
+	 $a_params_precs=$this->connexionBD->params();
+	 $a_types_a_creer = array();
+	 if (count($this->a_type_acte)>0)
      {
-       fwrite($pf,"$st_type_acte;$st_sigle\n");
-     }
-     fclose($pf);
-     usleep(500000);
-     chmod($st_fich_temp,0444);
-     usleep(500000);
-     $st_fich_temp=addslashes($st_fich_temp);
-     $st_requete="LOAD DATA $pst_parametres_load_data INFILE '$st_fich_temp' IGNORE INTO TABLE `type_acte` CHARACTER SET $gst_jeu_de_caracteres_par_defaut FIELDS TERMINATED BY '\;' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' (nom,sigle_nimegue)";
-     try
-     {
-       $this->connexionBD->execute_requete($st_requete);
-     }
-     catch (Exception $e) {
-       unlink($st_fich_temp);
-       die('Sauvegarde type_acte impossible: ' . $e->getMessage().": $st_requete ($st_fich_temp)");
-     }   
-     unlink($st_fich_temp);
+	   $st_requete = "insert ignore INTO `type_acte` (nom,sigle_nimegue) values ";
+       $a_colonnes = array();
+	   $i=0;
+	   foreach ($this->a_type_acte as $st_type_acte => $st_sigle )
+       {
+         $a_colonnes[] = "(:type$i,:sigle$i)";
+		 $a_types_a_creer[":type$i"]=$st_type_acte;
+		 $a_types_a_creer[":sigle$i"]=$st_sigle;
+         $i++; 
+	   }
+	   $st_colonnes = join(',',$a_colonnes);
+	   $st_requete .= $st_colonnes;
+	   try
+	   {
+		 $this->connexionBD->initialise_params($a_types_a_creer);  
+         $this->connexionBD->execute_requete($st_requete);
+		 $this->connexionBD->initialise_params($a_params_precs);
+       }
+       catch (Exception $e) {
+         die('Sauvegarde TypeActe impossible: ' . $e->getMessage().": $st_requete");
+      }
+	 } 
    }
    
    public function charge_liste_idf_par_nom() {
