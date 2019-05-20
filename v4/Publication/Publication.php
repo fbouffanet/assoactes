7c4ce0b558bd45eb542ca740dac16a58ec7ced88
pascal_frebot <agc-info@genea16.net>
2019-05-20 22:46:01 +0200

    date deb date fin

--- v4/Publication/Publication.php
+++ v4/Publication/Publication.php
@@ -547,16 +547,13 @@ switch($gst_mode)
       print('</div>');	 
      print('</div>');
    
-/*
+
 // Rajout PL dates d?but et fin *****************************
 print('<div align=center>Année début <input type=text size=4 name=pl_date_debut>');
 print(' Année fin <input type=text size=4 name=pl_date_fin></div><br><br>');
 //*********************************************************
-
-	//
-
-	//Ne fonctionne pas !!!!
-*/
+/*
+	 //Ne fonctionne pas !!!!
 	 
      //print('<div class="form-row">');
       print('<div class="form-group col-md-2">');
@@ -569,7 +566,7 @@ print(' Année fin <input type=text size=4 name=pl_date_fin></div><br><br>');
       print('</div>');
      //print('</div>');
 	
-	
+*/	
 
 print('</select></div><br>');
      print('<div align=center><input type="button" value="Création de la Publication" onClick="Exporte(0,\'EXPORTV3\')"/></div><br>');
