22e9008d8b5f7ebc35a61164422763bb037e9395
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-05-23 10:17:53 +0000

    Premier commit

---
+++ v4/Administration/chargement.php
@@ -0,0 +1,239 @@
+<?php
+
+/**
+ * Retourne vrai si la chaine est un patronyme (chaine majuscule)
+ * @param string $pst_chaine Chaine Ã“ vÃšrifier
+ * @return boolean vrai si $pst_chaine est en majuscules 
+ */ 
+ function est_patronyme($pst_chaine)
+ {
+    return $pst_chaine==strtoupper($pst_chaine);
+ }
+
+/**
+ * Retourne vrai si la chaine est un prÃšnom (Tous les prÃšnoms simples en minuscules sauf l'initiale en majuscule). Exemple :  Jean-Louis
+ * ! est considÃšrÃš comme un prÃšnom valide (non lisible) 
+ * @param string $pst_chaine Chaine Ã“ vÃšrifier
+ * @return boolean vrai si $pst_chaine est un prÃšnom
+ */ 
+ function est_prenom($pst_chaine)
+ {
+    if ($pst_chaine=='!') return true;
+    $a_champs = explode('-',$pst_chaine);
+    foreach ($a_champs as $st_champ)
+    {
+       if ($st_champ!=ucfirst(strtolower($st_champ)) || !preg_match("/^[A-Za-zaàäçèéëïöùü]+$/",strval($st_champ)))
+           return false;
+    }
+    return true;
+ }
+
+/** Renvoie les informations du conjoint tirees d'un champ nimegue V2
+ * @param string $pst_conjoint
+ * @return array(nom_conjoint, prenom_conjoint, commentaires conjoints)  
+ */
+function infos_conjoint($pst_conjoint)
+{  
+  $st_patro_conjoint='';
+  $st_prenom_conjoint='';
+  $st_cmt_conjoint='';
+  $st_lib_dec='';
+  $pst_conjoint = eregi_replace("^Veuve de ","",$pst_conjoint); 
+  $pst_conjoint = eregi_replace("^Veuf de ","",$pst_conjoint); 
+  $pst_conjoint = eregi_replace("^Veuf ","",$pst_conjoint); 
+  $pst_conjoint = eregi_replace("^Veuve ","",$pst_conjoint);
+  $pst_conjoint = eregi_replace("^Vve ","",$pst_conjoint);
+  $pst_conjoint = eregi_replace("^Vf ","",$pst_conjoint);
+  if (ereg(',',$pst_conjoint))
+  {
+     list($st_restant,$st_cmt_conjoint)=split(",",$pst_conjoint,2);
+     $pst_conjoint=$st_restant;
+  }
+  
+  if ($pst_conjoint!='')
+  {
+     if  (ereg("\x86",$pst_conjoint))
+     {
+         list($pst_conjoint,$st_lib_dec) = split("\x86",$pst_conjoint,2);
+         $pst_conjoint=rtrim($pst_conjoint);
+     }
+     $a_champs =  preg_split ('/\s+/',$pst_conjoint);
+     if (count($a_champs)==2)
+     {
+        if (est_patronyme($a_champs[0]) && est_prenom($a_champs[1]))
+        {
+           // le conjoint est de la forme : NOM PrÃšnom
+           $st_patro_conjoint = $a_champs[0];
+           $st_prenom_conjoint = $a_champs[1];
+        }
+        elseif (est_patronyme($a_champs[1]) && est_prenom($a_champs[0]))
+        {
+           // le conjoint est de la forme : PrÃšnom NOM 
+           $st_patro_conjoint = $a_champs[1];
+           $st_prenom_conjoint = $a_champs[0];
+        }
+        else
+        {
+          $st_cmt_conjoint .= $pst_conjoint;
+          $st_patro_conjoint = '#';
+        }    
+     }
+     else if (count($a_champs)==1)
+     {
+       if (est_patronyme($a_champs[0]))
+          $st_patro_conjoint = $a_champs[0];
+     }
+     else
+     {
+       // Le champ comporte plus de deux mots 
+       $a_patro_possible = array();
+       $a_prenom_possible = array();
+       $a_inconnus = array();
+       $a_autres = array();
+       /*
+       foreach ($a_champs as $st_champ)
+       { 
+         if ($st_champ=='?')
+            $a_inconnus[]='?';
+         else if (est_patronyme($st_champ))
+            $a_patro_possible[] = $st_champ;
+         else if (est_prenom($st_champ))
+            $a_prenom_possible[] = $st_champ;
+         else
+            $a_autres[] = $st_champ;
+       }
+       */
+       $st_champ = array_shift($a_champs);
+       $b_patro_init = false;
+       $b_prenom_init = false;
+       while (!is_null($st_champ))
+       {
+          if ($st_champ=='?')
+          {
+            $a_inconnus[]='?';
+            $st_champ = array_shift($a_champs);
+          }
+          else if (est_patronyme($st_champ))
+          { 
+             // Un patronyme a déjà été trouvé => echec de la conversion
+             if ($b_patro_init)
+             {
+                array_push($a_autres,$a_champs);
+                break;
+             }
+             $b_patro_init = true; 
+             while (!is_null($st_champ) && est_patronyme($st_champ))
+             {
+                //print("PAT=$st_champ\n");
+                $a_patro_possible[] = $st_champ;
+                $st_champ = array_shift($a_champs);   
+             }
+          }
+          else if (est_prenom($st_champ))
+          {
+             // Un prénom a déjà été trouvé => echec de la conversion
+             if ($b_prenom_init)
+             {
+                array_push($a_autres,$a_champs);
+                break;
+             }
+             $b_prenom_init = true; 
+             while (!is_null($st_champ) && est_prenom($st_champ))
+             {
+                //print("PRN=$st_champ\n"); 
+                $a_prenom_possible[] = $st_champ;
+                $st_champ = array_shift($a_champs);
+             }
+          }
+          else
+          {
+             $a_autres[] = $st_champ;
+             $st_champ = array_shift($a_champs); 
+          }  
+                   
+       }
+       // rÃšcupÃšration d'un prÃšnom inconnu si un nom est possible
+       if (count($a_prenom_possible)==0 && count($a_inconnus)==1 && count($a_patro_possible)>=1)
+          $a_prenom_possible[] = '?'; 
+       if (count($a_autres)==0 && count($a_patro_possible)>=1 && count($a_prenom_possible)>=1)
+       {
+          $st_patro_conjoint = join(' ',$a_patro_possible);
+          $st_prenom_conjoint = join(' ',$a_prenom_possible);
+       }
+       else
+       {      
+         $st_cmt_conjoint .= $pst_conjoint;
+         $st_patro_conjoint = '#';
+       }  
+     }  
+  }
+  if ($st_lib_dec!='')
+      $st_cmt_conjoint.= "\x86$st_lib_dec";
+  nettoie_prenom($st_prenom_conjoint);
+  nettoie_commentaires($st_cmt_conjoint);
+  return array($st_patro_conjoint,$st_prenom_conjoint,$st_cmt_conjoint);
+}
+/**
+ * Renvoie vrai si l'acte comporte un Ãšlement pouvait apporter une information autre que le nom ou le prÃ©nom, faux sinon
+ *
+ * @param unknown_type $pa_champs Champs Ã“ regarder
+ * @param unknown_type $pi_indice1 index du premier champ dÃšcrivant l'individu 1
+ * @param unknown_type $pi_indice2 index du premier champ  dÃšcrivant l'individu 2
+ * @param unknown_type $pi_longueur_personnes longueur des champs dÃšcrivant un individu
+ * @return boolean vrai si l'acte est filiatif
+ */
+
+function est_informatif($pa_champs,$pi_indice1,$pi_indice2,$pi_longueur_personnes)
+{
+   $st_chaine = join('',array_slice($pa_champs,$pi_indice1,$pi_longueur_personnes));
+   $st_chaine .= join('',array_slice($pa_champs,$pi_indice2,$pi_longueur_personnes));
+   #  on considÃžre que l'acte ne comporte pas d'informations si tous ses champs sont vides
+   return !preg_match('/^[\s\!\?]*$/',$st_chaine);
+}
+
+/**
+  *  Rend un prÃšnom propre (Exemple "JEAn-eMILE d'ALENCON" => "Jean-Emile D\'Alencon")
+  *  @param string $pst_prenom
+ */ 
+function nettoie_prenom(&$pst_prenom)
+{
+	$pst_prenom = ucwords(strtolower($pst_prenom));
+	foreach (array('-', '\'', ' ') as $delimiter)
+	{
+	  if (strpos($pst_prenom , $delimiter)!==false)
+	  {
+		$pst_prenom = addslashes(implode($delimiter, array_map('ucfirst', explode($delimiter, $pst_prenom))));
+	  }
+	}
+}
+
+/**
+ *  Rend un nom propre (Example "d'Elbauve" => "D'ELBAUVE")
+ *  @param string $pst_nom
+ */ 
+function nettoie_nom(&$pst_nom)
+{    
+   $pst_nom = strtoupper(addslashes($pst_nom));
+}
+
+/**
+ * Nettoie les commentaires  (suppression des ')
+ * @param string  $pst_commentaires commentaire Ã“ nettoyer 
+ */ 
+function nettoie_commentaires(&$pst_commentaires)
+{   
+   $pst_commentaires=addslashes($pst_commentaires);
+}
+
+/*
+ * Renvoie une chaine sans guillemets
+ * @param string $pst_chaine
+ * return $pst_chaine
+*/
+function sans_guillemets($pst_chaine)
+{
+  return str_replace('"','',$pst_chaine);
+}
+
+//print_r(infos_conjoint("Catherine DE MARSAT"));
+?>
---
+++ v4/Stats/StatsAdhesion.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html> 
+<html lang="fr">
+<head>
+  <title>Statistiques des adhésions</title>
+  <meta charset="iso-8859-15">       <!-- ou charset="utf-8" -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta http-equiv="content-language" content="fr">
+<script src='Commun/jquery-min.js' type='text/javascript'></script>
+<script src='Commun/menu.js' type='text/javascript'></script>
+    
+</head>
+
+<body>
+<?php
+//http://127.0.0.1:8888/Stats_Adhesion.php
+/*
+Programme d'affichage du nombre d'adhérents d'une année et du montant des adhésions 
+réparti en paiement par internet et chéque
+PL 05/14
+*/
+
+$gst_chemin = "../";
+
+require_once("$gst_chemin/Commun/config.php");
+require_once("$gst_chemin/Commun/constantes.php");
+require_once("$gst_chemin/Commun/Identification.php");
+
+// La page est reservee uniquement aux gens ayant les droits utilitaires
+require_once("$gst_chemin/Commun/VerificationDroits.php");
+
+verifie_privilege(DROIT_STATS);
+require_once("$gst_chemin/Commun/ConnexionBD.php");
+require_once("$gst_chemin/Commun/PaginationTableau.php");
+require_once("$gst_chemin/Commun/commun.php");
+
+function Mois_Annee ()  // Function pour affichage du mois en français
+{
+   $mois = array('', 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+   $mois_numero = date("n");   
+   $mois_complet = $mois[$mois_numero];
+   $jour = date("d");
+   $annee = date("Y");
+   return $jour." ".$mois_complet." ".$annee;
+}
+
+
+/* --- Cumule et affiche les résultats --- */
+function Affiche_Stats()
+{
+  global $connexionBD;
+  $annee = (int) $_POST['annee'];
+  $ga_nb_cartes = array();
+  $ga_nb_cheques= array();
+  $ga_tarifs = array(15,33,43);
+  foreach ($ga_tarifs as $i_tarif)
+  {
+     $ga_nb_cartes[$i_tarif]=0;
+     $ga_nb_cheques[$i_tarif]=0;
+  }
+  
+  $st_requete = "select prix, jeton_paiement, concat(prenom,' ',nom,' (',idf,')') from adherent where annee_cotisation = $annee and prix != 0";
+  $date_jour = Mois_Annee();
+  print("<div CLASS=TITRE>Statistiques du nombre et montant des adh&eacute;sions pour l'ann&eacute;e $annee au $date_jour</div>");
+  $a_adhesions=$connexionBD->sql_select_multiple($st_requete);
+  foreach ( $a_adhesions as $a_adh)
+  {
+    list($i_prix,$st_jeton,$st_adherent) = $a_adh;
+    if (in_array($i_prix,$ga_tarifs))
+    {
+      $st_jeton = trim($st_jeton);
+      if (empty($st_jeton))
+        $ga_nb_cheques[$i_prix]++;
+     else
+        $ga_nb_cartes[$i_prix]++;
+    }
+    else
+       print("<div class=IMPORTANT>Tarif $i_prix inexistant pour l'adh&eacute;rent $st_adherent</div>");
+   }
+   print("<form action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");  
+	 print("<br><br><div align=center>");
+	
+   
+   print("</div>");
+	
+	print("<div align=center>");
+	print("<table border=1 cellpadding=5>");
+	print("<tr><th>&nbsp;</th>");
+  foreach ($ga_tarifs as $i_tarif)
+  {
+    print("<th>Adh&eacute;sion $i_tarif euros</th>");
+  }	
+	print("<th>&nbsp;</th></tr>");
+	print("<tr>");
+	print("<td> Paiement par chèque </td>");
+  $gi_nb_tot_cheques = 0;
+  $gi_tot_cheques=0;
+  foreach ($ga_nb_cheques as $i_tarif => $i_nb_cheques)
+  {
+     $i_montant = $i_nb_cheques *$i_tarif;
+     print(sprintf("<td>%d adh&eacute;sions pour %d euros </td>",$i_nb_cheques,$i_montant));
+     $gi_nb_tot_cheques+=$i_nb_cheques;
+     $gi_tot_cheques+=$i_montant;  
+  }
+  print(sprintf("<td>Soit %d adh&eacute;sions pour %d euros </td>",$gi_nb_tot_cheques,$gi_tot_cheques));
+	print("</tr>");
+	print("<tr>");
+	print("<td> Paiement par Internet </td>");
+  $gi_nb_tot_cartes = 0;
+  $gi_tot_cartes=0;
+  foreach ($ga_nb_cartes as $i_tarif => $i_nb_cartes)
+  {
+     $i_montant = $i_nb_cartes*$i_tarif;
+     print(sprintf("<td>%d adh&eacute;sions pour %d euros </td>",$i_nb_cartes,$i_montant));
+     $gi_nb_tot_cartes+=$i_nb_cartes;
+     $gi_tot_cartes+=$i_montant;  
+  }
+  print(sprintf("<td>Soit %d adh&eacute;sions pour %d euros </td>",$gi_nb_tot_cartes,$gi_tot_cartes));
+
+	print("<tr>");
+	print("<td> Soit au total </td>");
+  $gi_nb_tot_adhesions = 0;
+  $gi_tot_adhesions=0;
+  foreach ($ga_tarifs as $i_tarif)
+  {
+     $i_montant=$ga_nb_cheques[$i_tarif]*$i_tarif+$ga_nb_cartes[$i_tarif]*$i_tarif;
+     print(sprintf("<td>%d adh&eacute;sions pour %d euros </td>",$ga_nb_cheques[$i_tarif]+$ga_nb_cartes[$i_tarif],$i_montant));
+     $gi_nb_tot_adhesions+=$ga_nb_cheques[$i_tarif]+$ga_nb_cartes[$i_tarif];
+     $gi_tot_adhesions+=$i_montant;
+  }
+  print(sprintf("<td>Soit %d adh&eacute;sions pour %d euros </td>",$gi_nb_tot_adhesions,$gi_tot_adhesions)); 
+	print("</tr>");
+	print("</table><br>");
+	
+	print("<p><input type=submit value='Retour' name='retour_annee'></p>");  
+  print("<input type=hidden name=mode value=\"DEPART\">");
+  print("</div><br>");
+  print("</form>");	
+}
+
+/* --- Saisie de l'année à afficher --- */
+
+function Saisie_annee()
+{
+   global $connexionBD;
+   $a_annees = $connexionBD->sql_select( $st_requete = "SELECT DISTINCT (`annee_cotisation`) FROM `adherent` ORDER BY `annee_cotisation`"); 
+   print("<form action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");  
+	 print("<br><br><div align=center>");
+   print("<p><h2>Statistiques du nombre et montant des adhésions</h2></p><br>");
+   print("</div><br>");
+   print("<div align=center>");
+   print("<select name=annee>");
+	 print chaine_select_options_simple('',$a_annees);
+	 print("</select>"); 
+	 print("<p><input type=submit value='Valider' name='valide_annee'></p>");  
+   print("<input type=hidden name=mode value=\"AFFICHE\">");
+   print("</div><br>");
+   print("</form>");
+}
+
+/* --- Début du programme --- */
+print("<link href='../Commun/Styles.css' type='text/css' rel='stylesheet'>");
+print("<script src='../Commun/jquery-min.js' type='text/javascript'></script>");
+print("<script src='../Commun/menu.js' type='text/javascript'></script>");
+$connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
+require_once("$gst_chemin/Commun/menu.php");
+
+$gst_mode = empty($_POST['mode']) ? 'DEPART': $_POST['mode'] ;
+$gi_num_page_cour = empty($_GET['num_page']) ? 1 : $_GET['num_page'];
+
+switch ($gst_mode) 
+{
+   case 'DEPART' : 
+      Saisie_annee(); 
+   break;
+   case 'AFFICHE' : 
+		Affiche_Stats($connexionBD); 
+   break;
+}
+
+
+//=====================================================================================
+// requête SQL qui compte le nombre  d'adhérent par année
+
+$st_requete = "SELECT a.annee_cotisation,count(*),sum(case when jeton_paiement !='' then 1 else 0 end)  FROM `adherent` a where a.statut in ('B','I') group by a.annee_cotisation order by a.annee_cotisation desc";
+echo "<div class=TITRE>Nbrs Adh(B+I) année de cotisation</div><br>"; 
+$pagination = new PaginationTableau($_SERVER['PHP_SELF'],'stats_adhesions',3,NB_LIGNES_PAR_PAGE,DELTA_NAVIGATION,array('Ann&eacute;e','Nbrs','Adh&eacute;sions<br> en ligne'));
+$pagination->init_param_bd($connexionBD,$st_requete);
+$pagination->init_page_cour($gi_num_page_cour);
+$pagination->affiche_tableau_simple_requete_sql();
+
+//=================================================================================== 
+// début du tableau Adhésion par mois
+// requête SQL Comptage des demandes par mois
+$st_requete = "SELECT YEAR(date_paiement)as annee, MONTH(date_paiement)as mois, COUNT(*)as nombre FROM adherent WHERE `statut`IN ('B','I') GROUP BY YEAR(date_paiement) desc,MONTH(date_paiement) desc ";
+echo "<br><div class=TITRE>Adh&eacute;sion par mois </div><br>";
+$pagination = new PaginationTableau($_SERVER['PHP_SELF'],'stats_adhesions',3,NB_LIGNES_PAR_PAGE,DELTA_NAVIGATION,array('Ann&eacute;e','Mois','Nbrs'));
+$pagination->init_param_bd($connexionBD,$st_requete);
+$pagination->init_page_cour($gi_num_page_cour);
+$pagination->affiche_tableau_simple_requete_sql();
+ 
+//====================================================================================== 
+// Nbrs Adh par statut et année de cotisation
+  
+$st_requete = 'SELECT a.annee_cotisation,sa.nom,count(*) FROM `adherent` a  join `statut_adherent` sa on (sa.idf=a.statut) group by a.annee_cotisation,a.statut order by a.annee_cotisation desc,a.statut'; 
+echo "<div class=TITRE>Nbrs Adh par statut et année de cotisation</div><br>";
+$pagination = new PaginationTableau($_SERVER['PHP_SELF'],'stats_adhesions',3,NB_LIGNES_PAR_PAGE,DELTA_NAVIGATION,array('Ann&eacute;e','Statut','Nbrs'));
+$pagination->init_param_bd($connexionBD,$st_requete);
+$pagination->init_page_cour($gi_num_page_cour);
+$pagination->affiche_tableau_simple_requete_sql();
+
+//Répartition des Adhérents  
+
+// Combien en France
+$st_requete = ("SELECT COUNT( * ) as Nbrs , left( cp, 2 ) as Departement FROM `adherent` WHERE `pays` LIKE 'france' AND `statut` IN ('B', 'I')GROUP BY left( cp, 2 )");
+echo "<div class=TITRE>Adh&eacute;rents par d&eacute;partement</div><br>";
+$pagination = new PaginationTableau($_SERVER['PHP_SELF'],'stats_adhesions',3,NB_LIGNES_PAR_PAGE,DELTA_NAVIGATION,array('Nbrs','D&eacute;p'));
+$pagination->init_param_bd($connexionBD,$st_requete);
+$pagination->init_page_cour($gi_num_page_cour);
+$pagination->affiche_tableau_simple_requete_sql();
+
+
+// Combien hors de France
+$st_requete=("SELECT COUNT( * ) as Nbrs ,pays FROM `adherent` WHERE `pays` NOT LIKE 'france' AND `statut` IN ('B', 'I') GROUP BY pays order by pays");	
+echo "<div class=TITRE>Adh&eacute;rents hors de France</div><br>";
+$pagination = new PaginationTableau($_SERVER['PHP_SELF'],'stats_adhesions',3,NB_LIGNES_PAR_PAGE,DELTA_NAVIGATION,array('Nbrs','Pays'));
+$pagination->init_param_bd($connexionBD,$st_requete);
+$pagination->init_page_cour($gi_num_page_cour);
+$pagination->affiche_tableau_simple_requete_sql();
+
+?>
+</body>
+</html>
