88e2fa100e2bb6dfdec750327d27b7ca4f231581
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2019-06-06 17:16:31 +0200

    Update Publication.php
    purple-boder

--- v4/Publication/Publication.php
+++ v4/Publication/Publication.php
@@ -721,7 +721,7 @@ $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
 	print ('<p>');
 	if ($menuDIV == "O")
 	{
-	print ('<div class="form-group">');
+	print ('<div class="form-group purple-boder">');
     print ('<label for="TypeActe">Complément du type acte pour les actes divers expl=> Actes Notariés</label>');
     print ('<textarea class="form-control rounded-0" id="TypeActe" rows="1"></textarea>');
     print ('</div>');	
