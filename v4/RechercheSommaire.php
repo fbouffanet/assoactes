3a6cd528451cc8bbd2620f5629d6ed8989600a85
pascal_frebot <agc-info@genea16.net>
2019-06-09 21:56:44 +0200

    Update RechercheSommaire.php
    php3

--- v4/RechercheSommaire.php
+++ v4/RechercheSommaire.php
@@ -39,8 +39,8 @@ print('<meta http-equiv="content-language" content="fr">');
 
 
 
-print ("<script src='Commun/jquery-min.js' type='text/javascript'></script>");
-print ("<script src='Commun/menu.js' type='text/javascript'></script>");
+//print ("<script src='Commun/jquery-min.js' type='text/javascript'></script>");
+//print ("<script src='Commun/menu.js' type='text/javascript'></script>");
 
 print('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
 print("<link href='css/styles.css' type='text/css' rel='stylesheet'>");
