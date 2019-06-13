54fc72883a73e3a8adebd999055e12fca940d497
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-06-13 18:48:18 +0200

    Correction de la localisation de la bibliothèque de captcha lorsque l'on vient de inscription.genea16.net

--- v4/Inscription/index.php
+++ v4/Inscription/index.php
@@ -1,12 +1,12 @@
 <?php
 
-$gst_chemin = "$gst_rep_site";
+$gst_chemin = "http://adherents.genea16.net";
 
 require_once("$gst_chemin/Commun/config.php");
 require_once("$gst_chemin/Commun/constantes.php");
 require_once("$gst_chemin/Commun/ConnexionBD.php");
 require_once("$gst_chemin/Commun/commun.php");
-$cryptinstall="$gst_chemin/Commun/crypt/cryptographp.fct.php";
+$cryptinstall="$gst_rep_site/Commun/crypt/cryptographp.fct.php";
 include $cryptinstall;
 
 print('<!DOCTYPE html>');
