5f5bc0ef08ddb70ae53c0bf90b4688530c7816cf
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2019-06-17 17:34:16 +0200

    Revert "Revert "Update Publication.php""
    This reverts commit 6bdcd1c512e1b0cf6ab5376565dcd053ab3db046.

--- v4/Publication/Publication.php
+++ v4/Publication/Publication.php
@@ -703,7 +703,7 @@ $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
 	print ('<p>');
 	if ($menuDIV == "O")	{
 	print "<br><b>Compl&egrave;ment du type acte pour les actes divers expl=> Actes Notari&egrave;s</b></br>";
-	print ('<textarea name="TypeActe" rows="1" placeholder = "Actes Notari&egrave" cols="45"></textarea><br><br>');
+	print ('<textarea name="TypeActe" rows="1" placeholder = "Actes Notari&egrave;s" cols="45"></textarea><br><br>');
 	}
 	print "<br><b>Info sur la publication expl=> Relev&egrave; par:</b></br>";
     print ('<textarea name="message" rows="8" cols="45"></textarea><br>');
