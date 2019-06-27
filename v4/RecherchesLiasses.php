4dac382c73a1a9aee8b71363751e01e10bdf9a70
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-06-27 22:53:15 +0200

    Remplacement de AGC/Charente par des paramètres

--- v4/RecherchesLiasses.php
+++ v4/RecherchesLiasses.php
@@ -10,7 +10,7 @@ print("<head>");
 print('<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" >');
 print('<meta http-equiv="content-language" content="fr"> ');
 /* ------------------- modif title */
-print('<title>Base AGC: Vos recherches de liasses notariales</title>');
+print('<title>Base '.SIGLE_ASSO.': Vos recherches de liasses notariales</title>');
 print('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
 print("<link href='css/styles.css' type='text/css' rel='stylesheet'>");
 print("<link href='css/bootstrap.min.css' rel='stylesheet'>");
