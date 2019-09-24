f06760b1bda6ce09c0edbcf376810bef7ca74dbe
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2019-09-24 14:21:24 +0200

    Update aff_pdf.php
    anulation modif

--- v4/Publication/aff_pdf.php
+++ v4/Publication/aff_pdf.php
@@ -197,7 +197,7 @@ $data=$connexionBD->ligne_suivante_resultat($req);
  //echo $data[5]["commune"];
 
 //=============================================================================
-$commune = utf8_decode($data[2]);// nom de la commune
+$commune = $data[2];// nom de la commune
 $chaine = $data[1];// N° de la commune
 $titreN = substr ($chaine, strlen ($chaine) - 3);// N° de la commune
 $type_actes_nimegue = $data[5];// Type Acte (B,D,M,V)
