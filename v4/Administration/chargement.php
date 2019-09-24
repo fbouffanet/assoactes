5022057e4203d276ab58f38ad8fc4ebbab974ca5
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-09-25 00:16:37 +0200

    Ajout du copyright

--- v4/Administration/chargement.php
+++ v4/Administration/chargement.php
@@ -1,5 +1,11 @@
 <?php
 
+// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association Généalogique de la Charente)
+// Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes de la
+// Licence Publique Générale GPL GNU publiée par la Free Software Foundation
+// Texte de la licence : http://www.gnu.org/copyleft/gpl.html
+//-------------------------------------------------------------------‌
+
 /**
  * Retourne vrai si la chaine est un patronyme (chaine majuscule)
  * @param string $pst_chaine Chaine Ó vÚrifier
