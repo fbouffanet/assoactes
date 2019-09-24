602662b22fa08de7b407689ea569128d013b0ba6
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-09-25 00:00:12 +0200

    Ajout du copyright

--- v4/Commun/commun.php
+++ v4/Commun/commun.php
@@ -1,5 +1,11 @@
 <?php
 
+// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association Généalogique de la Charente)
+// Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes de la
+// Licence Publique Générale GPL GNU publiée par la Free Software Foundation
+// Texte de la licence : http://www.gnu.org/copyleft/gpl.html
+//-------------------------------------------------------------------?
+
 /**
 * Renvoie la chaine d'options dans un select HTML
 /* @param string $pst_idf_choisi identifiant de l'option choisie
