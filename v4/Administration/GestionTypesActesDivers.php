816a0f8d5272524bb7f4feb049fdad7b39bb5074
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-09-25 00:16:37 +0200

    Ajout du copyright

--- v4/Administration/GestionTypesActesDivers.php
+++ v4/Administration/GestionTypesActesDivers.php
@@ -1,4 +1,10 @@
 <?php
+// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association Généalogique de la Charente)
+// Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes de la
+// Licence Publique Générale GPL GNU publiée par la Free Software Foundation
+// Texte de la licence : http://www.gnu.org/copyleft/gpl.html
+//-------------------------------------------------------------------?
+
 //echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
 require_once '../Commun/config.php';
 require_once '../Commun/constantes.php';
