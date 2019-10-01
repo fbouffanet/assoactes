02c178ec4ce9d08d0a7bd1fdfc05a059dd7375c1
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-10-01 22:25:42 +0200

    Retour à l'environnement de production de paiement

--- v4/Adhesion/configuration/identification.php
+++ v4/Adhesion/configuration/identification.php
@@ -1,18 +1,18 @@
 <?php
   //HOMOLOGATION
-  DEFINE( 'MERCHANT_ID', '49941014591194' ); // Merchant ID
+  //DEFINE( 'MERCHANT_ID', '49941014591194' ); // Merchant ID
   //PRODUCTION
-  //DEFINE( 'MERCHANT_ID', '60291817202228' ); // Merchant ID
+  DEFINE( 'MERCHANT_ID', '60291817202228' ); // Merchant ID
   
   //HOMOLOGATION
-  DEFINE( 'ACCESS_KEY', 'hZYiqSiP3C7F0srHFpCc' ); // Certificate key
+  //DEFINE( 'ACCESS_KEY', 'hZYiqSiP3C7F0srHFpCc' ); // Certificate key
   //PRODUCTION
-  //DEFINE( 'ACCESS_KEY', 'drwO5szPyOVfskyaOTuf' ); // Certificate key
+  DEFINE( 'ACCESS_KEY', 'drwO5szPyOVfskyaOTuf' ); // Certificate key
 
 	DEFINE( 'PROXY_HOST', null); // Proxy URL (optional)
 	DEFINE( 'PROXY_PORT', null); // Proxy port number without 'quotes' (optional)
 	DEFINE( 'PROXY_LOGIN', '' ); // Proxy login (optional)
 	DEFINE( 'PROXY_PASSWORD', '' ); // Proxy password (optional)
 
-	DEFINE( 'PRODUCTION', FALSE); // Demonstration (FALSE) or production (TRUE) mode
+	DEFINE( 'PRODUCTION', TRUE); // Demonstration (FALSE) or production (TRUE) mode
 ?>
