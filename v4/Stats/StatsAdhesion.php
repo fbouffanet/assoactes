53781313310b00d0b4f6a6a8f5105780cd5c5a60
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-10-05 11:01:20 +0200

    Ajout du copyright

--- v4/Stats/StatsAdhesion.php
+++ v4/Stats/StatsAdhesion.php
@@ -17,7 +17,11 @@
 <body>
 <div class="container">
 <?php
-
+// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association Généalogique de la Charente)
+// Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes de la
+// Licence Publique Générale GPL GNU publiée par la Free Software Foundation
+// Texte de la licence : http://www.gnu.org/copyleft/gpl.html
+//-------------------------------------------------------------------
 $gst_chemin = "../";
 
 require_once("$gst_chemin/Commun/config.php");
