bc8ffbc7d7e01f271c6a4092d317c9e27d73da06
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-10-06 10:41:40 +0200

    Ajout du copyright

--- v4/CarteDepouillementsCM.php
+++ v4/CarteDepouillementsCM.php
@@ -102,11 +102,14 @@
 
 <g class="communes">
 <?php
+// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association Généalogique de la Charente)
+// Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes de la
+// Licence Publique Générale GPL GNU publiée par la Free Software Foundation
+// Texte de la licence : http://www.gnu.org/copyleft/gpl.html
+//-------------------------------------------------------------------
 require_once 'Commun/config.php';
 require_once 'Commun/constantes.php';
 require_once 'Commun/ConnexionBD.php';
-
-
 
 $a_communes = array();
 $connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
