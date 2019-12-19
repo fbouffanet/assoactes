8f8eaa6e7d0bde5a62cb89337a52dba4c6713f43
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2019-12-19 18:44:24 +0100

    Conversion en utf8

--- v4/Commun/commun.php
+++ v4/Commun/commun.php
@@ -8,12 +8,22 @@
 
 /*
 * Renvoie une chaine encodée en cp1252 en UTF8
-* @param string $st_valeur chaine à convertir
+* @param string $st_valeur chaine cp1252 à convertir
 * @return string chaine encodée en UTF8
 */
 function cp1252_vers_utf8($st_valeur)
 {
 	return mb_convert_encoding($st_valeur,'UTF8','cp1252');
+}
+
+/*
+* Renvoie une chaine encodée en UTF8 en cp1252
+* @param string $st_valeur chaine UTF8 à convertir
+* @return string chaine encodée en UTF8
+*/
+function utf8_vers_cp1252($st_valeur)
+{
+	return mb_convert_encoding($st_valeur,'cp1252','UTF8');
 }
 
 /**
