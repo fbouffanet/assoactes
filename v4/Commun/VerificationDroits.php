d35c9eb58abae7a470c6be8cd87807aad6c3b2e2
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2019-12-26 10:21:59 +0100

    Passage en UTF8

--- v4/Commun/VerificationDroits.php
+++ v4/Commun/VerificationDroits.php
@@ -1,20 +1,20 @@
 <?php
-// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association Généalogique de la Charente)
+// Copyright (C) : Fabrice Bouffanet 2010-2019 (Association GÃ©nÃ©alogique de la Charente)
 // Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes de la
-// Licence Publique Générale GPL GNU publiée par la Free Software Foundation
+// Licence Publique GÃ©nÃ©rale GPL GNU publiÃ©e par la Free Software Foundation
 // Texte de la licence : http://www.gnu.org/copyleft/gpl.html
 //-------------------------------------------------------------------
 
 /* config.php, Identification.php  et ConnexionBD doivent avoir ete charges auparavant*/
 
 /**
- * Indique si l'utilisateur identifié par $pst_ident bénéficie du droit $pst_droit  
+ * Indique si l'utilisateur identifiÃ© par $pst_ident bÃ©nÃ©ficie du droit $pst_droit  
  * @param string $pst_ident identifiant
- * @param string $st_droit droit à vérifier
- * @return boolean : vrai si l'utilisateur possède le droit indiqué 
+ * @param string $st_droit droit Ã  vÃ©rifier
+ * @return boolean : vrai si l'utilisateur possÃ¨de le droit indiquÃ© 
  * @global string $gst_utilisateur_bd utilisateur de la connexion BD
- * @global string $gst_mdp_utilisateur_bd mot de passe pour se connecter à la BD
- * @global string $gst_nom_bd nom de la base de données
+ * @global string $gst_mdp_utilisateur_bd mot de passe pour se connecter Ã  la BD
+ * @global string $gst_nom_bd nom de la base de donnÃ©es
  * @global string $gst_serveur_bd serveur de bd     
 */
 function a_droits($pst_ident,$pst_droit) 
@@ -28,7 +28,7 @@ function a_droits($pst_ident,$pst_droit)
 } 
 
 /**
- *  Verifie que l'utilisateur connecte a le droit $pst_droit pour visualiser la page, sinon arrête le script
+ *  Verifie que l'utilisateur connecte a le droit $pst_droit pour visualiser la page, sinon arrÃªte le script
  *  @param string $pst_droit
  **/ 
 function verifie_privilege($pst_droit)
