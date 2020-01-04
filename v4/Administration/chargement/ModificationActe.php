5f2c4c5571bc5a7e3cfcad2d674938998e1210a2
Fabrice Bouffanet <fabrice.bouffanret@club-internet.fr>
2020-01-04 11:08:10 +0100

    Changement du nom de script de consultation des actes

--- v4/Administration/chargement/ModificationActe.php
+++ v4/Administration/chargement/ModificationActe.php
@@ -628,7 +628,7 @@ public function accepte ($pi_idf_valideur, $pst_prenom_valideur, $pst_nom_valide
      $st_message = 'Bonjour,<br><br>';
      $st_message .= "Votre proposition de modification d'un acte de la base ".SIGLE_ASSO." vient d'&ecirc;tre accept&eacute;e<br>";
      $st_message .= "Vous pouvez consulter l'acte &agrave l'adresse suivante:<br>";
-     $st_message .= sprintf("<a href=\"%s/InfosTD.php?idf_acte=%d\">$gst_url_site/InfosTD.php?idf_acte=%d</a><br>", $gst_url_site, $this -> i_idf_acte, $this -> i_idf_acte);
+     $st_message .= sprintf("<a href=\"%s/InfosActe.php?idf_acte=%d\">$gst_url_site/InfosActe.php?idf_acte=%d</a><br>", $gst_url_site, $this -> i_idf_acte, $this -> i_idf_acte);
      if (! empty($pst_cmt_valideur))
          {
         $st_message .= "<br>Commentaires du valideur: $pst_cmt_valideur<br>";
