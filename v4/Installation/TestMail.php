da9805c456c3044314cdfefe91eb382d2cc65e61
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2020-01-13 23:20:21 +0100

    Ajout d'un test basique

--- v4/Installation/TestMail.php
+++ v4/Installation/TestMail.php
@@ -27,6 +27,58 @@ print("<script src='../js/bootstrap.min.js' type='text/javascript'></script>");
 ?>
 <script type='text/javascript'>
 $(document).ready(function() {
+	$("#mail_basique").validate({
+	rules: {
+     email_destinataire0: {"required": true,
+			 "email":true
+	 },
+	 email_origine0: {
+			 "email":true
+	 }
+	},
+	messages: {
+		email_destinataire0: {
+			required: "L'email est obligatoire",
+			email:"Ceci n'est pas un email"
+		},
+	},
+	errorElement: "em",
+	errorPlacement: function ( error, element ) {
+		// Add the `help-block` class to the error element
+		error.addClass( "help-block" );
+
+		// Add `has-feedback` class to the parent div.form-group
+		// in order to add icons to inputs
+		element.parents( ".col-md-8" ).addClass( "has-feedback" );
+
+		if ( element.prop( "type" ) === "checkbox" ) {
+				error.insertAfter( element.parent( "label" ) );
+		} else {
+				error.insertAfter( element );
+		}
+
+		// Add the span element, if doesn't exists, and apply the icon classes to it.
+		if ( !element.next( "span" )[ 0 ] ) {
+			 $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+		}
+	},
+	success: function ( label, element ) {
+		// Add the span element, if doesn't exists, and apply the icon classes to it.
+		if ( !$( element ).next( "span" )[ 0 ] ) {
+			 $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+		}
+	},
+	highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-md-8" ).addClass( "has-error" ).removeClass( "has-success" );
+			$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+	},
+	unhighlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-md-8" ).addClass( "has-success" ).removeClass( "has-error" );
+			$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+	}
+	});
+}); 
+$(document).ready(function() {
 	$("#mail_brut").validate({
 	rules: {
      email_destinataire1: {"required": true,
@@ -84,6 +136,24 @@ $(document).ready(function() {
 /* Affiche le menu */
 function affiche_menu()
 {
+	print('<form method="post" action='.$_SERVER['PHP_SELF'].' id="mail_basique">');
+	print('<div class="panel panel-primary">');
+	print("<div class=\"panel-heading\">Envoi d'un email basique</div>");
+	print('<div class="panel-body">');
+	print('<input name="mode" type="hidden" value="basique">');
+	print('<div class="form-group row">');  
+    print("<label for=\"email_origine0\" class=\"col-md-4 col-form-label control-label\">Email d'origine</label>");
+	print('<div class="col-md-8">');
+	print("<input type=text maxlength=40 size=40 name=email_origine0 id=email_origine0 value=\"".EMAIL_DIRASSO."\" class=\"form-control\">");
+    print('</div></div>');
+    print('<div class="form-group row">');  
+    print("<label for=\"email_destinataire0\" class=\"col-md-4 col-form-label control-label\">Email de destination</label>");
+	print('<div class="col-md-8">');
+	print("<input type=text maxlength=40 size=40 name=email_destinataire0 id=email_destinataire0 value=\"fbouffanet@yahoo.fr\" class=\"form-control\">");
+    print('</div></div>');
+	print('<button type="submit" class="btn btn-primary  col-md-offset-4 col-md-4""><span class="glyphicon glyphicon-wrench"></span> Envoyer un email basique sans ent&ecirc;te</button>'); 
+	print('</div></div>');
+	print('</form>');
 	print('<form method="post" action='.$_SERVER['PHP_SELF'].' id="mail_brut">');
 	print('<div class="panel panel-primary">');
 	print("<div class=\"panel-heading\">Envoi d'un email brut</div>");
@@ -168,6 +238,21 @@ else
     error_reporting( E_ALL );	
 	switch($_POST['mode'])
 	{
+		case 'basique':
+		
+		    $st_email_destinataire=trim($_POST['email_destinataire0']);
+            $st_email_origine=trim($_POST['email_origine0']);		
+			if (!empty($st_email_origine))
+				$st_entete  = "From: $st_email_origine\n";
+			if (mail($st_email_destinataire, "Test de mail brut", "Ceci est un message brut", $st_entete))
+				print("<div class=\"alert alert-success\"> Message envoy&eacute; &agrave; $st_email_destinataire</div>");
+			else
+			{
+				print("<div class=\"alert alert-danger\"> Echec lors de l'envoi du  message &agrave; $st_email_destinataire</div>");
+				print("<blockquote>".error_get_last()['message']."</blockquote>");	
+			}				
+			affiche_menu();
+		break;
 		case 'brut':
 		
 		    $st_email_destinataire=trim($_POST['email_destinataire1']);
@@ -184,7 +269,7 @@ else
 					$st_entete  .=" <$st_email_origine>";
 				$st_entete  .="\n>";
 			}
-			if (@mail($st_email_destinataire, "Test de mail brut", "Ceci est un message brut", $st_entete))
+			if (mail($st_email_destinataire, "Test de mail brut", "Ceci est un message brut", $st_entete))
 				print("<div class=\"alert alert-success\"> Message envoy&eacute; &agrave; $st_email_destinataire</div>");
 			else
 			{
@@ -208,7 +293,7 @@ else
 					$st_entete  .=" <$st_email_origine>";
 				$st_entete  .="\n>";
 			}
-			if (@mail($st_email_destinataire, "Test de mail HTML", "Ceci est un message HTML",$st_entete))
+			if (mail($st_email_destinataire, "Test de mail HTML", "Ceci est un message HTML",$st_entete))
 				print("<div class=\"alert alert-success\"> Message envoy&eacute; &agrave; $st_email_destinataire</div>");
 			else
 			{
@@ -244,7 +329,7 @@ else
 			$st_message .= 'Content-Transfer-Encoding: 8bit'."\n\n";
 			$st_message .= $st_message_html."\n\n";
 			$st_message .= '--'.$st_frontiere."--\n";
-			if (@mail($st_email_destinataire, "Test de mail HTML", "Ceci est un message HTML",$st_entete))
+			if (mail($st_email_destinataire, "Test de mail HTML", "Ceci est un message HTML",$st_entete))
 				print("<div class=\"alert alert-success\"> Message envoy&eacute; &agrave; $st_email_destinataire</div>");
 			else
 			{
