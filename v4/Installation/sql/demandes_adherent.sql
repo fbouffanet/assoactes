7f3f28b202b913eb8a8fadc6f5ad80bcc084dc1e
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2020-01-15 08:14:46 +0100

    Changement de la taille de l'adresse IP de demande

--- v4/Installation/sql/demandes_adherent.sql
+++ v4/Installation/sql/demandes_adherent.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS `demandes_adherent` (
   `idf` mediumint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifiant de la demande',
   `idf_adherent` mediumint(6) unsigned NOT NULL DEFAULT '0' COMMENT 'Identifiant de l''adherent',
-  `adresse_ip` varchar(15) COLLATE latin1_general_ci NOT NULL,
+  `adresse_ip` varchar(23) COLLATE latin1_general_ci NOT NULL,
   `idf_commune` smallint(5) unsigned NOT NULL COMMENT 'Commune de l''acte',
   `idf_acte` mediumint(11) unsigned DEFAULT NULL,
   `idf_type_acte` smallint(5) unsigned DEFAULT NULL,
