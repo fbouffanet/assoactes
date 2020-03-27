e7af4a75f7ea6bc57451fc210e687181d3b66230
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2020-03-27 16:15:27 +0100

    Update Publication.php

--- v4/Publication/Publication.php
+++ v4/Publication/Publication.php
@@ -1,5 +1,4 @@
 <?php
-//26-09 
 require_once '../Commun/config.php';
 require_once '../Commun/constantes.php';
 require_once('../Commun/Identification.php');
@@ -13,7 +12,7 @@ $gst_repertoire_publication = $_SERVER['DOCUMENT_ROOT'].'/v4/Publication/telecha
 
 print('<!DOCTYPE html>');
 print("<head>");
-print('<link rel="shortcut icon" href="images/favicon.ico">') ;
+print('<link rel="shortcut icon" href="images/favicon.ico">');
 print('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">');
 print('<meta http-equiv="content-language" content="fr">');
 print('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
