d6d583559ff79ca283aaa51ceccbf701f895c589
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2020-03-31 12:44:43 +0200

    Suppression d'un double appel d'une fonction de conversion

--- v4/AfficheStatsCommune.php
+++ v4/AfficheStatsCommune.php
@@ -174,8 +174,7 @@ switch ($gst_mode) {
         if (!empty($gst_commune_a_chercher))
         {
             $st_requete .= " where ca.nom like :recherche";
-			$gst_commune_a_chercher=utf8_vers_cp1252($gst_commune_a_chercher);
-            $connexionBD->initialise_params(array(":recherche"=>$gst_commune_a_chercher));       
+            $connexionBD->initialise_params(array(":recherche"=>utf8_vers_cp1252($gst_commune_a_chercher)));       
         }
      }
      else
@@ -184,8 +183,7 @@ switch ($gst_mode) {
         if (!empty($gst_commune_a_chercher))
         {
             $st_requete .= " and ca.nom like :recherche";
-			$gst_commune_a_chercher=utf8_vers_cp1252($gst_commune_a_chercher);
-            $connexionBD->initialise_params(array(":recherche"=>$gst_commune_a_chercher)); 
+            $connexionBD->initialise_params(array(":recherche"=>utf8_vers_cp1252($gst_commune_a_chercher))); 
         }
      }
      $st_requete .= " ORDER BY init";
@@ -261,7 +259,7 @@ switch ($gst_mode) {
        else
        {
           $st_requete ="select distinct ca.idf,ca.nom,ca.debut_communale,ca.debut_greffe from `commune_acte` ca join `stats_commune` sc on (ca.idf=sc.idf_commune) where sc.idf_source=$gi_idf_source and ca.nom like :recherche order by ca.nom";
-          $connexionBD->initialise_params(array(":recherche"=>$gst_commune_a_chercher));
+          $connexionBD->initialise_params(array(":recherche"=>utf8_vers_cp1252($gst_commune_a_chercher)));
         }
      }
      $a_liste_communes = $connexionBD->sql_select_multiple_par_idf($st_requete);
