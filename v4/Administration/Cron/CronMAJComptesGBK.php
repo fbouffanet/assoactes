25bd17a37eb7868aad08da89a3533e05f8947c7e
Fabrice BOUFFANET <fbouffanet@yahoo.fr>
2020-04-13 10:56:24 +0200

    Utilisation de la classe Courriel

--- v4/Administration/Cron/CronMAJComptesGBK.php
+++ v4/Administration/Cron/CronMAJComptesGBK.php
@@ -5,16 +5,19 @@
 // Texte de la licence : http://www.gnu.org/copyleft/gpl.html
 //-------------------------------------------------------------------
 
+
 require_once '/var/www/clients/client1/web3/web/v4/Commun/config.php';
 require_once '/var/www/clients/client1/web3/web/v4/Commun/constantes.php';
 require_once '/var/www/clients/client1/web3/web/v4/Commun/ConnexionBD.php';
 require_once '/var/www/clients/client1/web3/web/v4/Commun/Adherent.php';
+require_once '/var/www/clients/client1/web3/web/v4/Commun/Courriel.php';
 
 /*
 require_once '../../Commun/config.php';
 require_once '../../Commun/constantes.php';
 require_once '../../Commun/ConnexionBD.php';
 require_once '../../Commun/Adherent.php';
+require_once '../../Commun/Courriel.php';
 */
 
 $connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
@@ -26,21 +29,28 @@ foreach ($a_liste_idf as $i_idf)
 {
   $st_cmd_gbk .= "set ".PREFIXE_ADH_GBK.$i_idf." ".NB_POINTS_GBK."\r\n";
 }
-//print("<pre>$st_cmd_gbk</pre>");
+
 if (!Adherent::execute_cmd_gbk($st_cmd_gbk))
 {
     $st_message_erreur = Adherent::erreur_gbk();
     $st_requete = "select email_perso from adherent adht join privilege p on (adht.idf=p.idf_adherent) where p.droit='".DROIT_GENEABANK."' order by email_perso";
     $a_adresses = $connexionBD->sql_select($st_requete);
-    $st_entete  = "From: ".LIB_ASSO."<".EMAIL_DIRASSO.">\n>";
-    $st_entete .= "Reply-to: ".LIB_ASSO."<".EMAIL_DIRASSO.">\n";
-    $st_entete .= "Cc: ".EMAIL_DIRASSO."\n";
-    $st_entete .= "Reply-to: ".SIGLE_ASSO." <".EMAIL_DIRASSO.">\n";
-    $st_entete .= 'MIME-Version: 1.0' . "\n"; 
     $st_sujet = "Test Comptes GBK";
     $st_sujet = "Erreur lors de la maj des comptes GBK";
-    $st_adresses =  join(',',$a_adresses);        
-    mail($st_adresses,$st_sujet,$st_message_erreur, $st_entete);
+    $courriel = new Courriel($gst_rep_site,$gst_serveur_smtp,$gst_utilisateur_smtp,$gst_mdp_smtp,$gi_port_smtp);
+	$courriel->setExpediteur(EMAIL_DIRASSO,LIB_ASSO);
+    $courriel->setAdresseRetour(EMAIL_DIRASSO);
+	$courriel->setEnCopie(EMAIL_DIRASSO);
+	foreach ($ga_emails_gestbase as $st_email_destinataire)
+	{
+		$courriel->setDestinataire($st_email_destinataire,'');
+	}
+	$courriel->setSujet($st_sujet);
+	$courriel->setTexte($st_message_erreur);
+	if (!$courriel->envoie())
+	{
+		print("<div class=\"alert alert-danger\">Le message n'a pu être envoyé. Erreur: ".$courriel->get_erreur()."</div>");
+	}
 }
 else
 {
