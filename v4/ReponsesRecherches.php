5352c2d015d5ac50ab2cc41dbc38d093fea128c0
Fabrice Bouffanet <fbouffanet@yahoo.fr>
2020-04-17 17:25:57 +0200

    Correction de l'affichage des communes voisines

--- v4/ReponsesRecherches.php
+++ v4/ReponsesRecherches.php
@@ -163,7 +163,7 @@ function rappel_recherches_communes($pconnexionBD,$pst_titre,$pi_idf_type_acte,$
     }
     $st_criteres .= "Commune s&eacute;lectionn&eacute;e: ".cp1252_vers_utf8($st_nom_commune)."\n";
     $st_bloc_rappel = nl2br($st_criteres);
-    $st_communes_voisines=join("\n",cp1252_vers_utf8(array_values($requeteRecherche->communes_voisines())));
+    $st_communes_voisines=join("\n",array_values($requeteRecherche->communes_voisines()));
     if (count(array_values($requeteRecherche->communes_voisines()))>1)
     {
       $st_bloc_rappel .="<div class=\"form-group\"><label for=\"communes_voisines\">Paroisses voisines ou rattach&eacute;es";
@@ -172,7 +172,7 @@ function rappel_recherches_communes($pconnexionBD,$pst_titre,$pi_idf_type_acte,$
         $st_bloc_rappel .="(avec recherches dans un rayon de $pi_rayon km)\n";
       }
 	  $st_bloc_rappel .="</label>";
-      $st_bloc_rappel .="<textarea rows=6 cols=40 id=\"communes_voisines\" class=\"form-control\">$st_communes_voisines</textarea></div>";
+      $st_bloc_rappel .="<textarea rows=6 cols=40 id=\"communes_voisines\" class=\"form-control\">".cp1252_vers_utf8($st_communes_voisines)."</textarea></div>";
     }
     $st_bloc_rappel .="</div>";
     $st_bloc_rappel .="</div>";
