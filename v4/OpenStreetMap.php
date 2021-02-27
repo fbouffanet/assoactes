3dfdc0a34d0b4a2a4873f016545f39e4bb3cd37e
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-02-27 17:54:07 +0100

    Update OpenStreetMap.php

--- v4/OpenStreetMap.php
+++ v4/OpenStreetMap.php
@@ -100,7 +100,7 @@ catch (Exception $e) {
                 // Nous ajoutons un marqueur
                 var marker = L.marker([lat, lon]).addTo(macarte);
                 // Nous ajoutons la popup. A noter que son contenu (ici la variable ville) peut être du HTML
-				marker.bindPopup(ville);
+				marker.bindPopup(ville,' Longitude : 'lon,' Latitude : 'lat);
             }
            
             window.onload = function(){
