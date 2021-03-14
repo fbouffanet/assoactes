3d2a2c859c595c49c664941654885c7ebb9f9d0c
pascal_frebot <agc-info@genea16.net>
2021-03-14 20:20:13 +0100

    Update sql_recensement.sql

--- v4/Publication/sql_recensement.sql
+++ v4/Publication/sql_recensement.sql
@@ -3,20 +3,22 @@ p.idf_acte,
 p.idf,
 p.sexe, 
 p.patronyme,
-ifnull(prenom.libelle,''),
+Prenom as ifnull(prenom.libelle,''),
 p.idf_origine,
-c.nom,
-p.date_naissance,
-p.age,
-p.idf_profession, 
+Lieu_origine as ifnull(c.nom,''),
+date_naissance as ifnull(p.date_naissance,''),
+age as ifnull(p.age,''),
+profession as ifnull(d.profession,''), 
 p.commentaires,
 a.commentaires,
+CHAR_LENGTH(a.commentaires),
 a.url,
 a.details_supplementaires
 from 
 personne p 
 left join prenom on (p.idf_prenom=prenom.idf) 
-left join commune_personne c (p.idf_origine = idf)
+join commune_personne c on (p.idf_origine =c.idf)
+join profession d on (p.idf_profession =d.idf)
 join acte a on (p.idf_acte=a.idf)
 where a.idf_commune=208 and a.idf_source=1 and a.idf_type_acte=147 order by a.details_supplementaires
 
