5879f38e949b82754488049a83466986aefe1953
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-03-16 18:43:23 +0100

    Update Publication.php
    ajout des dates

--- v4/Publication/Publication.php
+++ v4/Publication/Publication.php
@@ -603,6 +603,7 @@ $date_fin = $row[1];
 if ($date_deb < 1500)
 {
    $sqltmp = "select annee from acte where idf_commune=$gi_idf_commune_acte and idf_source=$gi_idf_source and idf_type_acte=$gc_idf_type_acte order by annee";
+   ++++++++++++++++++++++++++++++++++++++++++++++++++++
    print $sqltmp ;
    print "</br>";
 	while ($row = $connexionBD-> sql_select($sqltmp)) {
@@ -621,6 +622,40 @@ $sqltmp = $sqltmp ." order by p.idf_acte,p.idf";
 $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
 //****************************************************************************
          break;
+
+//======================== RECENSEMENT DEB ============================================================
+      case IDF_RECENS :
+        $sqltmp = "select idf,idf_commune,idf_type_acte,date, date_rep, cote,libre, commentaires from acte where idf_commune=$gi_idf_commune_acte and idf_source=$gi_idf_source and idf_type_acte =147)";
+        if (!empty($g_pl_date_debut)) $sqltmp = $sqltmp . " and annee >= $g_pl_date_debut";
+        if (!empty($g_pl_date_fin)) $sqltmp = $sqltmp . " and annee <= $g_pl_date_fin";
+        $a_liste_actes= $connexionBD->sql_select_multiple_par_idf($sqltmp);
+        // Nombre de lignes s?lect?es
+        $results= $connexionBD->liste_valeur_par_clef($sqltmp);
+        $nb_rows = count($results);
+        // pour r?cup?rer l'ann?e mini et maxi
+          $sqltmp = "select min(annee) as annee_deb, max(annee) as annee_fin from acte where idf_commune=$gi_idf_commune_acte and idf_source=$gi_idf_source and idf_type_acte =147)";
+
+
+          if (!empty($g_pl_date_debut)) $sqltmp = $sqltmp . " and annee >= $g_pl_date_debut";
+          if (!empty($g_pl_date_fin)) $sqltmp = $sqltmp . " and annee <= $g_pl_date_fin";
+          // pour r?cup?rer les ann?es s?lectionn?es
+          $row = $connexionBD-> sql_select_liste($sqltmp);
+          $date_deb = $row[0];
+          $date_fin = $row[1];
+            // rajout test si date ? 0
+          if ($date_deb < 1500)
+          {
+            $sqltmp = "select min(annee) as annee_deb, max(annee) as annee_fin from acte where idf_commune=$gi_idf_commune_acte and idf_source=$gi_idf_source and idf_type_acte= 147)";
+            while ($row = $connexionBD-> sql_select($sqltmp)) {
+            if ($row[0] > 1500) {
+            $date_deb = $row[0];
+              break;
+              }    
+            }
+          }
+        
+//=============================  RECENSEMENT FIN ===============================================
+
 
       case IDF_DIVERS :
 // Rajout PL sur les dates ***********************************************************
