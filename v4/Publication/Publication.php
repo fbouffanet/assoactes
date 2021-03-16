08dbe89ab914f2458c0506dfc3d0da6c26fb5e1d
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-03-16 09:51:10 +0100

    Update Publication.php

--- v4/Publication/Publication.php
+++ v4/Publication/Publication.php
@@ -492,33 +492,7 @@ function export_div_nimv3($pconnexionBD,$pi_idf_source,$pi_idf_commune_acte,$pa_
 
 function export_recensement($pconnexionBD,$pi_idf_commune_acte)
 {
-   /*
-   $sql="select 
-   cast(substring(a.commentaires,INSTR(a.commentaires,"N° de page:")+12,3) as INT) as Page,
-   substring(a.commentaires,INSTR(a.commentaires,"Quartier:")+9,10) as Quartier,
-   substring(a.commentaires,INSTR(a.commentaires,"Nom de la Rue:")+14,10) as Rue,
-   cast(substring(a.commentaires,INSTR(a.commentaires,"N° maison:")+10,3)as INT) as Maison,
-   cast(substring(a.commentaires,INSTR(a.commentaires,"N° ménage:")+10,3)as INT) as Ménage,
-   p.patronyme as Nom,
-   ifnull(prenom.libelle,'') as Prénom,
-   ifnull(p.age,'') as Age,
-   right(p.date_naissance,4) as Année°,
-   c.nom as Lieu°,
-   ifnull(p.commentaires,'') as Observation,
-   a.url as Lien,
-   a.details_supplementaires
-   from 
-   personne p 
-   left join prenom on (p.idf_prenom=prenom.idf) 
-   join commune_personne c on (p.idf_origine =c.idf)
-   join profession d on (p.idf_profession =d.idf)
-   join acte a on (p.idf_acte=a.idf)
-   where a.idf_commune=208 and a.idf_source=1 and a.idf_type_acte=147 order by Page ASC, Maison ASC, Ménage ASC
-   INTO OUTFILE "$gst_repertoire_publication/ExportNimV3.csv"
-   FIELDS TERMINATED BY ':'
-   ENCLOSED BY '"'
-   LINES TERMINATED BY '\n'";
-   */
+
     
    
     //$a_liste_actes= $connexionBD->sql_select_multiple_par_idf($sql);
