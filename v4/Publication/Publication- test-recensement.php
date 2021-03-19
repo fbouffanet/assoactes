7e446dadff0f82aefb43229038366776d53cc932
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-03-19 11:55:57 +0100

    Update Publication- test-recensement.php
    ajout

--- v4/Publication/Publication- test-recensement.php
+++ v4/Publication/Publication- test-recensement.php
@@ -702,15 +702,15 @@ $nb_rows = count($results);
 print "<br>case</br>";
 print $nb_rows."<br></br>";
 break;
-break;
-/*
+
+
 // Rajout PL sur les dates ***********************************************************
 $sqltmp = "select p.idf_acte,p.idf,p.idf_type_presence,p.sexe, p.patronyme,ifnull(prenom.libelle,''),p.idf_origine,p.date_naissance,p.age,p.idf_profession, p.commentaires,p.idf_pere,p.idf_mere,p.est_decede from personne p left join prenom  on (p.idf_prenom=prenom.idf) join acte a on (p.idf_acte=a.idf)where a.idf_commune=$gi_idf_commune_acte and a.idf_source=$gi_idf_source and a.idf_type_acte=$gc_idf_type_acte";
 if (!empty($g_pl_date_debut)) $sqltmp = $sqltmp . " and annee >= '$g_pl_date_debut'";
 if (!empty($g_pl_date_fin)) $sqltmp = $sqltmp . " and annee <= '$g_pl_date_fin'";
 $sqltmp = $sqltmp ." order by p.idf_acte,p.idf";
 $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
- */           
+       
 
 //=============================  RECENSEMENT FIN ===============================================
       case IDF_DIVERS :
@@ -743,14 +743,14 @@ if ($date_deb < 1500)
 }
 //======================================================================================
 // Rajout PL sur les dates
-/*
+
 $sqltmp = "select p.idf_acte,p.idf,p.idf_type_presence,p.sexe, p.patronyme,ifnull(prenom.libelle,''),p.idf_origine,p.date_naissance,p.age,p.idf_profession, p.commentaires,p.idf_pere,p.idf_mere,p.est_decede from personne p left join prenom on (p.idf_prenom=prenom.idf) join acte a on (p.idf_acte=a.idf) where a.idf_commune=$gi_idf_commune_acte and a.idf_source=$gi_idf_source and a.idf_type_acte not in (".IDF_NAISSANCE.",".IDF_MARIAGE.",".IDF_DECES.",".IDF_RECENS.")";
 if (!empty($g_pl_date_debut)) $sqltmp = $sqltmp . " and annee >= $g_pl_date_debut";
 if (!empty($g_pl_date_fin)) $sqltmp = $sqltmp . " and annee <= $g_pl_date_fin";
 $sqltmp = $sqltmp ." order by p.idf_acte,p.idf";
 
 $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
-*/
+
    }
 
 // PL fichier des dates saisies pour le script aff_pdf ******************************
