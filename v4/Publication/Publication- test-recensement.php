de7a9eabd5948c088e237bf697cd037b47161a25
pascal_frebot <agc-info@genea16.net>
2021-03-19 22:22:03 +0100

    Update Publication- test-recensement.php

--- v4/Publication/Publication- test-recensement.php
+++ v4/Publication/Publication- test-recensement.php
@@ -513,10 +513,11 @@ order by Année_Recensement ASC,Page ASC, Maison ASC, Ménage ASC";
 
 
 
-  list($Année_Recensement,$Page,$Quartier,$Rue,$Maison,$Ménage,$Nom,$Prénom,$Age,$Année°,$Lieu°,$Observation,$Lien,$st_nom_commune) = $pconnexionBD->liste_valeur_par_clef($sqltmp);
+  //list($Année_Recensement,$Page,$Quartier,$Rue,$Maison,$Ménage,$Nom,$Prénom,$Age,$Année°,$Lieu°,$Observation,$Lien,$st_nom_commune) = $pconnexionBD->liste_valeur_par_clef($sqltmp);
+  
   //$a_profession=$pconnexionBD->liste_valeur_par_clef("select idf, nom from profession");
-
-  foreach ($pa_liste_personnes as $i_idf_acte => $a_champs)
+  $valeur=$pconnexionBD->liste_valeur_par_clef($sqltmp);
+  foreach ($valeur as $a_champs)
   {
      $a_champs = array();
      //foreach ($a_personnes as $i_idf_personne => $a_personne)
