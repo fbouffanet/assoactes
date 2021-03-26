e9def5784c3e225a4a0d5b38325c1c7424027ed4
pascal_frebot <agc-info@genea16.net>
2021-03-26 22:09:59 +0100

    Update Publication- test-recensement.php

--- v4/Publication/Publication- test-recensement.php
+++ v4/Publication/Publication- test-recensement.php
@@ -708,7 +708,7 @@ $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
 
 //======================== RECENSEMENT DEB ============================================================
       case IDF_RECENS :
-        print "IDF_RECENS ligne 702 <br></br>";
+        print "IDF_RECENS ligne 711 <br></br>";
         print "gi_idf_commune_acte = ".$gi_idf_commune_acte."<br></br>";
         print "pi_idf_source = ".$gi_idf_source."<br></br>";
         print "pc_idf_type_acte = ".$gc_idf_type_acte."<br></br>";
@@ -730,6 +730,7 @@ $row = $connexionBD-> sql_select_liste($sqltmp);
 $date_deb = $row[0];
 $date_fin = $row[1];
 // rajout test si date ? 0
+print "ligne 732";
 if ($date_deb < 1500)
 {
    $sqltmp = "select annee from acte where idf_commune=$gi_idf_commune_acte and idf_source=$gi_idf_source and idf_type_acte=$gc_idf_type_acte order by annee";
