2a0b0b76ceddc136e8a81aac3d5a0bf22f257dfc
pascal_frebot <agc-info@genea16.net>
2021-03-28 22:01:06 +0200

    Update Publication- test-recensement.php

--- v4/Publication/Publication- test-recensement.php
+++ v4/Publication/Publication- test-recensement.php
@@ -459,7 +459,7 @@ function export_recensement($pconnexionBD, $pi_idf_source, $pi_idf_commune_acte,
       join commune_personne c on (p.idf_origine =c.idf)
       join profession d on (p.idf_profession =d.idf)
       join acte a on (p.idf_acte=a.idf)
-      where a.idf_commune= '$pi_idf_commune_acte' and a.idf_source='$pi_idf_source'and a.idf_type_acte='$psc_idf_type_acte'";
+      where a.idf_commune= '$pi_idf_commune_acte' and a.idf_source='$pi_idf_source'and a.idf_type_acte='$pc_idf_type_acte'";
 
 
   //$sqltmp = "select * from adherent order by nom, prenom";
