84a70326a7b7e3f5e1edd8988791ac0af8c9085f
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-04-05 11:03:14 +0200

    Update Publication- test-recensement.php

--- v4/Publication/Publication- test-recensement.php
+++ v4/Publication/Publication- test-recensement.php
@@ -462,7 +462,9 @@ function export_recensement($pconnexionBD, $pi_idf_source, $pi_idf_commune_acte,
       order by 'Annee_Recensement' ASC, 'Page' ASC, 'Maison' ASC, 'Menage' ASC";
 
 
-  //$sqltmp = "SELECT *  FROM `acte` WHERE `idf_commune` = 208 AND `idf_type_acte` = 147 AND `idf_source` = 1 ";
+
+  $sqltmp = "select p.idf_acte, p.idf, a.annee as Annee_Recensement, cast(substring(a.commentaires,INSTR(a.commentaires,'N de page:')+12,3) as INT) as Page, substring(a.commentaires,INSTR(a.commentaires,'Quartier')+9,10) as Quartier, substring(a.commentaires,INSTR(a.commentaires,'Nom de la Rue:')+14,10) as Rue, cast(substring(a.commentaires,INSTR(a.commentaires,'N° maison:')+10,3)as INT) as Maison, cast(substring(a.commentaires,INSTR(a.commentaires,'N° ménage:')+10,3)as INT) as Menage, p.patronyme as Nom, ifnull(prenom.libelle,'') as Prenom, ifnull(p.age,'') as Age, right(p.date_naissance,4) as Annee°, c.nom as Lieu°, d.nom as Profession from personne p left join prenom on (p.idf_prenom=prenom.idf) join commune_personne c on (p.idf_origine =c.idf) join profession d on (p.idf_profession =d.idf) join acte a on (p.idf_acte=a.idf) where a.idf_commune= 208 and a.idf_source=1 and a.idf_type_acte= 147 order by 'Annee_Recensement' ASC, 'Page' ASC, 'Maison' ASC, 'Menage' ASC";
+  
   print('<div class="alert alert-info">');
   print $sqltmp; // affichage de la requ�te
   print('</div>'); //  
