faec50fc4ae76e4fac7d23b47387c17e903059ea
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-04-08 10:45:17 +0200

    Update Publication_test_recensement.php

--- v4/Publication/Publication_test_recensement.php
+++ v4/Publication/Publication_test_recensement.php
@@ -813,7 +813,74 @@ switch ($gst_mode) {
       WHERE a.idf_commune= $gi_idf_commune_acte AND a.idf_source=$gi_idf_source AND a.idf_type_acte= $gc_idf_type_acte  
       ";
       //=================================================
-      $sqltmp="SELECT p.idf_acte, p.idf, 'R' as Sigle, f.nom AS Commune, a.annee AS Annee_Recensement, CAST(SUBSTRING(a.commentaires,INSTR(a.commentaires,'N de page:')+12,3) AS INT) AS Page, SUBSTRING(a.commentaires,INSTR(a.commentaires,'Quartier')+9,10) AS Quartier, SUBSTRING(a.commentaires,INSTR(a.commentaires,'Nom de la Rue:')+14,10) AS Rue, CAST(SUBSTRING(a.commentaires,INSTR(a.commentaires,'N° maison:')+10,3)AS INT) AS Maison, CAST(SUBSTRING(a.commentaires,INSTR(a.commentaires,'N° ménage:')+10,3)AS INT) AS Menage, p.patronyme AS Nom, IFNULL(prenom.libelle,'') AS Prenom, IFNULL(p.commentaires,''), IFNULL(p.age,'') AS Age, RIGHT(p.date_naissance,4) AS Annee°, c.nom AS Lieu°, d.nom AS Profession FROM personne p LEFT JOIN prenom ON (p.idf_prenom=prenom.idf) JOIN commune_personne c ON (p.idf_origine=c.idf) JOIN profession d ON (p.idf_profession=d.idf) JOIN acte a ON (p.idf_acte=a.idf) JOIN commune_acte f ON (a.idf_commune=f.idf) WHERE a.idf_commune= $gi_idf_commune_acte AND a.idf_source=$gi_idf_source AND a.idf_type_acte= $gc_idf_type_acte";
+      $sqltmp="SELECT
+      p.idf_acte,
+      p.idf,
+      'R' AS Sigle,
+      f.nom AS Commune,
+      a.annee AS Annee_Recensement,
+      CAST(
+          SUBSTRING(
+              a.commentaires,
+              INSTR(a.commentaires, 'N de page:') +12,
+              3
+          ) AS INT
+      ) AS PAGE,
+      SUBSTRING(
+          a.commentaires,
+          INSTR(a.commentaires, 'Quartier') +9,
+          10
+      ) AS Quartier,
+      SUBSTRING(
+          a.commentaires,
+          INSTR(a.commentaires, 'Nom de la Rue:') +14,
+          10
+      ) AS Rue,
+      CAST(
+          SUBSTRING(
+              a.commentaires,
+              INSTR(a.commentaires, 'N° maison:') +10,
+              3
+          ) AS INT
+      ) AS Maison,
+      CAST(
+          SUBSTRING(
+              a.commentaires,
+              INSTR(a.commentaires, 'N° ménage:') +10,
+              3
+          ) AS INT
+      ) AS Menage,
+      p.patronyme AS Nom,
+      IFNULL(prenom.libelle, '') AS Prenom,
+      IFNULL(p.commentaires, ''),
+      IFNULL(p.age, '') AS Age,
+      RIGHT(p.date_naissance, 4) AS Annee°,
+      c.nom AS Lieu°,
+      d.nom AS Profession
+  FROM
+      personne p
+  LEFT JOIN
+      prenom
+  ON
+      (p.idf_prenom = prenom.idf)
+  JOIN
+      commune_personne c
+  ON
+      (p.idf_origine = c.idf)
+  JOIN
+      profession d
+  ON
+      (p.idf_profession = d.idf)
+  JOIN
+      acte a
+  ON
+      (p.idf_acte = a.idf)
+  JOIN
+      commune_acte f
+  ON
+      (a.idf_commune = f.idf)
+  WHERE
+      a.idf_commune= $gi_idf_commune_acte AND a.idf_source=$gi_idf_source AND a.idf_type_acte= $gc_idf_type_acte";
       //============================================================================//
       
       print "ligne 711<br>";
@@ -821,7 +888,11 @@ switch ($gst_mode) {
       print "<br></br>";
         if (!empty($g_pl_date_debut)) $sqltmp = $sqltmp . " and annee >= '$g_pl_date_debut'";
         if (!empty($g_pl_date_fin)) $sqltmp = $sqltmp . " and annee <= '$g_pl_date_fin'";
-        $sqltmp = $sqltmp . "order by Annee_Recensement ASC, Page ASC, Maison ASC, Menage ASC";
+        $sqltmp = $sqltmp . " ORDER BY
+        Annee_Recensement ASC,
+        PAGE ASC,
+        Maison ASC,
+        Menage ASC";
         //$sqltmp= "select * from acte";  // requete de test
         print "requete ligne 715 ".$sqltmp."<br>"; 
         $a_liste_personnes = $connexionBD->liste_valeur_par_doubles_clefs($sqltmp);
