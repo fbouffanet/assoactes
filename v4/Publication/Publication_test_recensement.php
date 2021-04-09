8b196e22eb603ca3e7300dac1642db24543932d6
pascal_frebot <agc-info@genea16.net>
2021-04-09 22:32:38 +0200

    Update Publication_test_recensement.php

--- v4/Publication/Publication_test_recensement.php
+++ v4/Publication/Publication_test_recensement.php
@@ -472,11 +472,11 @@ function export_recensement($pconnexionBD, $pi_idf_source, $pi_idf_commune_acte,
            3
        ) AS INT
    ) AS Page,
-   SUBSTRING(
+   LTRIM(SUBSTRING(
        a.commentaires,
        INSTR(a.commentaires, 'Quartier') +9,
        10
-   ) AS Quartier,
+   )) AS Quartier,
    SUBSTRING(
        a.commentaires,
        INSTR(a.commentaires, 'Nom de la Rue:') +14,
