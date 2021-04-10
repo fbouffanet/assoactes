7a1709cfa7878d363188f7e5ecd4fde56eb5bb61
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-04-10 15:15:46 +0200

    Update Publication_test_recensement.php

--- v4/Publication/Publication_test_recensement.php
+++ v4/Publication/Publication_test_recensement.php
@@ -465,7 +465,7 @@ function export_recensement($pconnexionBD, $pi_idf_source, $pi_idf_commune_acte,
    f.nom AS Commune,
    a.annee AS Annee_Recensement,
    'R' AS Sigle,
-   REPLACE(a.commentaires,chr(13),';'),
+  TRIM(a.commentaires),
    CAST(
        SUBSTRING(
            a.commentaires,
