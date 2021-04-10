fd738d63b6c37c72a455be76b450ddbbf50d3a72
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-04-10 09:28:54 +0200

    Update Publication_test_recensement.php

--- v4/Publication/Publication_test_recensement.php
+++ v4/Publication/Publication_test_recensement.php
@@ -472,11 +472,11 @@ function export_recensement($pconnexionBD, $pi_idf_source, $pi_idf_commune_acte,
            3
        ) AS INT
    ) AS Page,
-   REPLACE(SUBSTRING(
+   SUBSTRING(
        a.commentaires,
        INSTR(a.commentaires, 'Quartier') +9,
        10
-   ),'\n','',) AS Quartier,
+   ) AS Quartier,
    TRIM('\n' FROM SUBSTRING(
        a.commentaires,
        INSTR(a.commentaires, 'Nom de la Rue:') +14,
