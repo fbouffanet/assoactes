0f61244da8c45ed743f2966f38d2d14eb423b934
pascal_frebot_fixe <pascal.frebot@neuf.fr>
2021-04-12 14:47:00 +0200

    Update Publication_test_recensement.php

--- v4/Publication/Publication_test_recensement.php
+++ v4/Publication/Publication_test_recensement.php
@@ -497,7 +497,7 @@ function export_recensement($pconnexionBD, $pi_idf_source, $pi_idf_commune_acte,
 	SUBSTRING(
 			REPLACE	(a.commentaires,CHAR(10),' '),
 					(INSTR(REPLACE(a.commentaires,CHAR(10),' '),'ménage:')+7),
-					(INSTR(REPLACE(a.commentaires,CHAR(10),' '),'de page:')-6)-(INSTR(REPLACE(a.commentaires,CHAR(10),' '),'ménage:')+6)
+					(INSTR(REPLACE(a.commentaires,CHAR(10),' '),'de page:')-4)-(INSTR(REPLACE(a.commentaires,CHAR(10),' '),'ménage:')+6)
 				  ) 
 	 AS Menage,
 	
