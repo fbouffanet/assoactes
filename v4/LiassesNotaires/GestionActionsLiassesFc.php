e97bb05bfca6b915d76c6c8008d9e1443339cac1
pascale crine <pascale.crine@orange.fr>
2021-04-25 17:09:35 +0200

    Update GestionActionsLiassesFc.php

--- v4/LiassesNotaires/GestionActionsLiassesFc.php
+++ v4/LiassesNotaires/GestionActionsLiassesFc.php
@@ -152,26 +152,26 @@ function menu_liste_releve($pconnexionBD)
 		$pagination->affiche_tableau_edition_sil(2);
 		//$pagination->affiche_entete_liens_navigation();      
 		print("<div align=center><input type=hidden name=mode value=\"SUPPRIMER_RELEVE\">");
-		print("<button type=submit name=\"SUPPRIMER_RELEVE\" class=\"btn btn-sm btn-outline-danger\" ONCLICK=\"VerifieSuppressionReleves(0,'supp[]')\">");
+		print('<div class="btn-group col-md-2 col-md-offset-1" role="group">');
+		print("<button type=submit name=\"SUPPRIMER_RELEVE\" class=\"btn btn-sm btn-danger\" ONCLICK=\"VerifieSuppressionReleves(0,'supp[]')\">");
 		print("    <span class=\"glyphicon glyphicon-trash\"></span>Supprimer les relevés sélectionnés</button>");
 		print('</div>');
 	}
 	else
 		print("<div align=center>Pas de relev&eacute;</div>\n");
 	print("</form>");  
-	print('<div class="btn-group col-md-2 col-md-offset-1" role="group">');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");  
 	print("<div align=center><input type=hidden name=mode value=\"MENU_AJOUTER_RELEVE\">");
-	print("<button type=submit name=Ajouter class=\"btn btn-sm btn-outline-success\">");
+	print("<button type=submit name=Ajouter class=\"btn btn-sm btn-success\">");
 	print("    <span class=\"glyphicon glyphicon-new-window\"></span> Ajouter un relevé</button>");
 	print('</div></form>');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");  
 	print("<div align=center><input type=hidden name=mode value=\"MENU_AJOUTER_RELEVEUR\">");
-	print("<button type=submit name=Ajouter class=\"btn btn-sm btn-outline-warning\">");
+	print("<button type=submit name=Ajouter class=\"btn btn-sm btn-warning\">");
 	print("    <span class=\"glyphicon glyphicon-new-window\"></span> Ajouter un releveur</button>");
-	print("</div>");  
+	print("</div><div align=center>______________________________________________________________________</div>");  
 	print('</form>');
-	print('</div><div align=center>______________________________________________________________________</div>');
+	print('</div>');
 }
 
 /**
