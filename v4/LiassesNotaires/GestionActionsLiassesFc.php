f000b4ac8bf47af4627c47888948b9151f919d55
pascale crine <pascale.crine@orange.fr>
2021-04-25 17:56:32 +0200

    Update GestionActionsLiassesFc.php

--- v4/LiassesNotaires/GestionActionsLiassesFc.php
+++ v4/LiassesNotaires/GestionActionsLiassesFc.php
@@ -333,7 +333,7 @@ function menu_gerer($pconnexionBD)
 	print('</form></div></div>');
 	print("<form  action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">");
 	print("<div align=center><input type=hidden name=mode value=\"LISTE\">");
-	print("<button type=submit name=LISTE class=\"btn btn-sm btn-success\">");
+	print("<button type=submit name=LISTE class=\"btn btn-sm btn-primary\">");
 	print("    <span class=\"glyphicon glyphicon-new-window\"></span>  Retour</button>");
 	print('</div></form>');
 }
