ba40b585c6f1e28f4ac45c206ea9a44fb3373650
pascale crine <pascale.crine@orange.fr>
2021-05-03 18:53:36 +0200

    Update GestionLiassesNotarialesFc.php

--- v4/LiassesNotaires/GestionLiassesNotarialesFc.php
+++ v4/LiassesNotaires/GestionLiassesNotarialesFc.php
@@ -143,10 +143,12 @@ function menu_edition($pst_cote, $pst_libelle, $pst_periodes, $pst_notaires,
 	if( $pst_mode == 'M' ) {
 		print('<div class="form-row col-md-12">'.
 					'<div class="form-group col-md-4" align="right"><label class="col-form-label">Cote de la liasse&nbsp</label></div>'.
-					'<div class="form-group col-md-1"><span class="input-group-addon" align="left">'.$pst_cote.'</span></div></div></div>');
+					'<div align="left">'.$pst_cote.'</div></div>');
+//					'<div class="form-group col-md-1"><span class="input-group-addon" align="left">'.$pst_cote.'</span></div></div></div>');
 		print('<div class="form-row col-md-12">'.
 					'<div class="form-group col-md-4" align="right"><label class="col-form-label">Période(s)&nbsp</label></div>'.
-					'<div class="form-group col-md-3"><span class="input-group-addon">'.$pst_periodes.'</span></div>'.
+					'<div >'.$pst_periodes.'</div>'.
+//					'<div class="form-group col-md-3"><span class="input-group-addon">'.$pst_periodes.'</span></div>'.
 					"<div class='form-group' align=''><a href='".$_SERVER['PHP_SELF']."?smode=LISTE_PERIODE&cote_liasse=".$pst_cote."'>".
 					"<img src='".$st_icone_info."' border=0 alt='détail des périodes'></a></div></div>");
 		print('<div class="form-row col-md-12">'.
