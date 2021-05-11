176722809ff76df82a1d3089322e175b1cfbd1f0
pascale crine <pascale.crine@orange.fr>
2021-05-11 20:33:42 +0200

    Update GestionLiassesNotariales.php

--- v4/LiassesNotaires/GestionLiassesNotariales.php
+++ v4/LiassesNotaires/GestionLiassesNotariales.php
@@ -86,38 +86,7 @@ $("#maj_liasses").validate({
 		"depose_ad":		{	integer: "Le département doit être renseigné pour une liasse déposée aux AD"	},
 		"dept_depose_ad":	{	annee_min_sans_periode: "La case 'Déposée aux AD' doit être cochée quand le département est renseigné"	},                                                                                              
 		"forme_liasse":		{	notaire_sans_notaire: "La forme de la liasse est obligatoire"	}
-  },
-  errorElement: "em",
-  errorPlacement: function ( error, element ) {
-	// Add the `help-block` class to the error element
-	error.addClass( "help-block" );
-
-	// Add `has-feedback` class to the parent div.form-group
-	// in order to add icons to inputs
-	element.parents( ".lib_erreur" ).addClass( "has-feedback" );
-
-	if ( element.prop( "type" ) === "checkbox" )	{	error.insertAfter( element.parent( "label" ) );	} 
-	else 											{	error.insertAfter( element );					}
-
-	// Add the span element, if doesn't exists, and apply the icon classes to it.
-	if ( !element.next( "span" )[ 0 ] ) {
-			$( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
-		}
-	},
-	success: function ( label, element ) {
-		// Add the span element, if doesn't exists, and apply the icon classes to it.
-		if ( !$( element ).next( "span" )[ 0 ] ) {
-			$( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
-		}
-	},
-	highlight: function ( element, errorClass, validClass ) {
-		$( element ).parents( ".lib_erreur" ).addClass( "has-error" ).removeClass( "has-success" );
-		$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
-	},
-	unhighlight: function ( element, errorClass, validClass ) {
-		$( element ).parents( ".lib_erreur" ).addClass( "has-success" ).removeClass( "has-error" );
-		$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
-	}
+  }
 });
 
 });
