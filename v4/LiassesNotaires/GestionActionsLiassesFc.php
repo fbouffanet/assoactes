507e967e9f6d5c2fadd25dbec193836788187370
pascale crine <pascale.crine@orange.fr>
2021-05-26 10:44:11 +0200

    Update GestionActionsLiassesFc.php

--- v4/LiassesNotaires/GestionActionsLiassesFc.php
+++ v4/LiassesNotaires/GestionActionsLiassesFc.php
@@ -177,6 +177,7 @@ function menu_liste_publication($pconnexionBD){
 				  "order by publication_papier.date_publication";
 	$a_liste_liasses = $pconnexionBD->sql_select_multipleUtf8($st_requete);
 	print('<div align=center><form name=menuPubli id=menuPubli action="'.$_SERVER['PHP_SELF'].'" method="post">');
+	print('<input type=hidden name=mode id=mode value="SUPPRIMER_LIEN_PUBLI">');
 	$i_nb_liasses = count($a_liste_liasses);
 	if ($i_nb_liasses!=0)
 	{        
@@ -186,7 +187,6 @@ function menu_liste_publication($pconnexionBD){
 		$pagination->init_param_bd($pconnexionBD,$st_requete);
 		$pagination->init_page_cour($gi_num_page_cour);
 		$pagination->affiche_tableau_edition_sil(2);
-		print('<input type=hidden name=mode value="SUPPRIMER_LIEN_PUBLI">');
 		print('<div class="btn-group col-md-9 col-md-offset-3" role="group">');
 		print("<button type=submit name=btSupprimerLienPubli id=btSupprimerLienPubli class='btn btn-sm btn-danger' ONCLICK=\"VerifieSuppressionLiensPublis(0,'supp[]')\">");
 		print('    <span class="glyphicon glyphicon-trash"></span>  Supprimer les liens publications sélectionnés</button>');
@@ -219,6 +219,7 @@ function menu_liste_photo($pconnexionBD){
 				  "order by liasse_photo.date_photo";
 	$a_liste_liasses = $pconnexionBD->sql_select_multipleUtf8($st_requete);
 	print('<div align=center><form name=menuPhoto id=menuPhoto action="'.$_SERVER['PHP_SELF'].'" method="post">');
+	print('<input type=hidden name=mode id=mode value="SUPPRIMER_PHOTO">');
 	$i_nb_liasses=count($a_liste_liasses);
 	if ($i_nb_liasses!=0)
 	{        
@@ -228,7 +229,6 @@ function menu_liste_photo($pconnexionBD){
 		$pagination->init_param_bd($pconnexionBD,$st_requete);
 		$pagination->init_page_cour($gi_num_page_cour);
 		$pagination->affiche_tableau_edition_sil(2);
-		print('<input type=hidden name=mode value="SUPPRIMER_PHOTO">');
 		print('<div class="btn-group col-md-9 col-md-offset-3" role="group">');
 		print("<button type=submit name='SUPPRIMER_PHOTO' class='btn btn-sm btn-danger' ONCLICK=\"VerifieSuppressionPhotos(0,'supp[]')\">");
 		print('    <span class="glyphicon glyphicon-trash"></span>  Supprimer les photos sélectionnées</button>');
@@ -263,6 +263,7 @@ function menu_liste_program($pconnexionBD){
 				  "order by liasse_programmation.date_creation";
 	$a_liste_liasses = $pconnexionBD->sql_select_multipleUtf8($st_requete);
 	print('<div align=center><form name=menuProgram id=menuProgram action="'.$_SERVER['PHP_SELF'].'" method="post">');
+	print('<input type=hidden name=mode id=mode value="SUPPRIMER_PROGRAM">');
 	$i_nb_liasses=count($a_liste_liasses);
 	if ($i_nb_liasses!=0)
 	{        
@@ -273,7 +274,6 @@ function menu_liste_program($pconnexionBD){
 		$pagination->init_param_bd($pconnexionBD,$st_requete);
 		$pagination->init_page_cour($gi_num_page_cour);
 		$pagination->affiche_tableau_edition_sil(2);
-		print('<input type=hidden name=mode value="SUPPRIMER_PROGRAM">');
 		print('<div class="btn-group col-md-9 col-md-offset-3" role="group">');
 		print("<button type=submit name='SUPPRIMER_PROGRAM' class='btn btn-sm btn-danger' ONCLICK=\"VerifieSuppressionPrograms(0,'supp[]')\">");
 		print('    <span class="glyphicon glyphicon-trash"></span> Supprimer les programmations sélectionnées</button>');
