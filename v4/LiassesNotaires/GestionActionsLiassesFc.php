46ff0ac36978e17c6f7858ca71c5e4bc4a78e481
pascale crine <pascale.crine@orange.fr>
2021-05-29 11:09:28 +0200

    test

--- v4/LiassesNotaires/GestionActionsLiassesFc.php
+++ v4/LiassesNotaires/GestionActionsLiassesFc.php
@@ -147,7 +147,7 @@ function menu_liste_releve($pconnexionBD){
 											array('Releveur','Date fin relevé','Publication numérique','Infos complémentaires','Modifier','Supprimer'));
 		$pagination->init_param_bd($pconnexionBD,$st_requete);
 		$pagination->init_page_cour($gi_num_page_cour);
-		$pagination->affiche_tableau_edition_sil(1, true);
+		$pagination->affiche_tableau_edition_sil(1, false);
 		print('<div class="btn-group col-md-9 col-md-offset-3" role="group">');
 		print("<button type=submit class='btn btn-sm btn-danger' id='btSupprimerReleve' ONCLICK=\"VerifieSuppressionReleves(0,'supp[]')\">");
 		print("    <span class=\"glyphicon glyphicon-trash\"></span>  Supprimer les relevés sélectionnés</button>");
