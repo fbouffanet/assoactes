e5c6f12462734dbb22cab0da5671c2ce8ed72757
pascale crine <pascale.crine@orange.fr>
2021-05-31 00:33:42 +0200

    Update GestionLiassesNotariales.php

--- v4/LiassesNotaires/GestionLiassesNotariales.php
+++ v4/LiassesNotaires/GestionLiassesNotariales.php
@@ -156,7 +156,7 @@ $("#btAjoutLiasse").click(function() {
 $("#btSupprimerLiasse").click(function() {
 	var chaine="";
 	// Un seul élément
-	alert(document.forms["listeLiasses"].elements.length);
+	alert(document.forms["listeLiasses"].elements[5].length);
 	if (document.forms['listeLiasses'].elements['supp'].checked)	{
 		chaine+=document.forms['listeLiasses'].elements['supp'].id;
 	}
