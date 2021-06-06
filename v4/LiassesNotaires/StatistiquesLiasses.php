fefd94a10741332eb2232ebd838f64d63a23dcc5
pascale crine <pascale.crine@orange.fr>
2021-06-06 11:45:04 +0200

    Update StatistiquesLiasses.php

--- v4/LiassesNotaires/StatistiquesLiasses.php
+++ v4/LiassesNotaires/StatistiquesLiasses.php
@@ -187,7 +187,8 @@ print('<div class="panel-heading">Statistiques sur les liasses</div>');
 print('<div class="panel-body" align="center">');
 print('<div class="alert alert-info">Série AD16 - 2E</div>');
 //print("<table border='1' cellpadding='0' cellspacing='0' align='center' width='720'><caption class='TITRE'>AD16 - 2E</caption>");
-print('<div align="center"><table class="table table-bordered">');
+print('<div class="row"><div class="col-2"> </div><div class="col-8">');
+print('<table class="table table-bordered">');
 print('<tr class="table-primary">'.
       '<th align="center" width="150">Nombre de liasses</th>'.
       '<th align="center" width="40">Avant 1793</th>'.
@@ -331,7 +332,7 @@ foreach ($a_liasses as $a_liasse) {
 		  '<td align="center" width="20%">'.$i_nb_autre_total.'</td>'.
 		  '</tr>');
 	}
-print('</table ></div></div></div>');
+print('</table ></div></div></div></div></div>');
 print('<br><div align="center" style="font-size:11px;color:#4f6b72"><i>Liasses relevées : liasses dont les CM ont été retranscrits</i></div>');
 print("</div></body></html>");
 ?>
